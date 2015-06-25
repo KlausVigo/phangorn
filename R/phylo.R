4c30438fac26c24dcfa22031075bade9dfc7db44
KlausVigo <klaus.schliep@gmail.com>
2015-06-25 12:40:23 -0400

    clean up code

--- R/phylo.R
+++ R/phylo.R
@@ -317,7 +317,7 @@ AICc <- function (object, ...)
 AICc.pml <- function(object, ...){
     n = sum(object$weight)
     k = object$df
-    if(k>=(n-1))return(NULL)    
+#    if(k>=(n-1))return(NULL)    
     res = AIC(object)
     res +   (2*k*(k+1))/(n-k-1)    
 }
@@ -1261,107 +1261,6 @@ optimEdge <- function (tree, data, eig=eig, w=w, g=g, bf=bf, rate=rate, ll.0=ll.
     if(control$trace>0) cat(start.ll, " -> ", newll, "\n")
     list(tree=treeP, logLik=newll, c(eps, iter))
 }
-
-
-optimEdgeOld <- function (tree, data, eig=eig, w=w, g=g, bf=bf, rate=rate, ll.0=ll.0,
-                        control = pml.control(epsilon = 1e-08, maxit = 10, trace=0), ...) 
-{
-    if (is.null(attr(tree, "order")) || attr(tree, "order") == "cladewise") 
-        tree <- reorder(tree, "postorder") 
-    nTips <- length(tree$tip)
-    el <- tree$edge.length
-    tree$edge.length[el < 0] <- 1e-08
-    oldtree = tree
-    k = length(w)    
-    data = subset(data, tree$tip) 
-    loglik = pml.fit2(tree, data, bf=bf, g=g, w=w, eig=eig, ll.0=ll.0, k=k)
-    start.ll <- old.ll <- loglik 
-    contrast <- attr(data, "contrast")
-    contrast2 <- contrast %*% eig[[2]] 
-    evi = (t(eig[[3]]) * bf)
-    weight <- attr(data, "weight")
-    eps = 1
-    iter = 0
-    
-    treeP = tree
-    tree = reorder(tree)
-    el <- tree$edge.length
-    
-    child = tree$edge[, 2]
-    parent = tree$edge[, 1]
-    loli = parent[1]
-    
-    pvec <- integer(max(tree$edge))
-    pvec[child] <- parent
-    
-    EL = numeric(max(child))
-    EL[child] = tree$edge.length
-    
-    nTips = min(parent) - 1
-    n = length(tree$edge.length)  
-    lt = length(tree$tip)
-    
-    nr = as.integer(length(weight))
-    nc = as.integer(length(bf))
-    nco = as.integer(nrow(contrast))
-    eve = eig[[2]]
-    lg = k
-    rootNode = getRoot(tree)         
-    ScaleEPS = 1.0/4294967296.0
-    anc = Ancestors(tree, 1:max(tree$edge), "parent")        
-    
-    while (eps > control$eps && iter < control$maxit) {
-        blub3 <- .Call("extractScale", as.integer(rootNode), w, g, as.integer(nr), as.integer(nc), as.integer(nTips))
-        rowM = apply(blub3, 1, min)       
-        blub3 = (blub3-rowM) 
-        blub3 = ScaleEPS ^ (blub3) 
-        
-        for(j in 1:n) {       
-            ch = child[j]
-            pa = parent[j]
-            
-            while(loli != pa){      
-                blub <- .Call("moveloli", as.integer(loli), as.integer(anc[loli]), eig, EL[loli], w, g, as.integer(nr), as.integer(nc), as.integer(nTips))
-                loli=anc[loli] 
-            } 
-            
-            old.el = tree$edge.length[j] 
-            if (old.el < 1e-8) old.el <- 1e-8
-            
-            X <- .Call("moveDad", data, as.integer(pa), as.integer(ch), eig, evi, old.el, w, g, as.integer(nr), as.integer(nc), as.integer(nTips), as.double(contrast), as.double(contrast2), nco)                
-            # in moveDad, das sollte teuer sein in Kopien
-            for(i in 1:k)X[[i]] <- X[[i]] * blub3[,i]
-            
-            newEL <- .Call("FS5", eig, nc, as.double(old.el), as.double(w), as.double(g), X, as.integer(length(w)), as.integer(length(weight)), as.double(bf), as.double(weight), as.double(ll.0))         
-            
-            el[j] = newEL[[1]] # nur EL?
-            EL[ch] = newEL[[1]]     
-            if (child[j] > nTips) loli  = child[j]
-            else  loli  = parent[j]    
-            
-            blub <- .Call("updateLL", data, as.integer(pa), as.integer(ch), eig, newEL[[1]], w, g,
-                          as.integer(nr), as.integer(nc), as.integer(nTips), as.double(contrast), nco)
-            
-            tree$edge.length = el
-        }
-        tree$edge.length = el
-        iter = iter + 1
-        
-        treeP$edge.length = EL[treeP$edge[,2]]
-        newll <- pml.fit2(treeP, data, bf=bf, g=g, w=w, eig=eig, ll.0=ll.0, k=k)
-        
-        eps = ( old.ll - newll ) / newll
-        if( eps <0 ) return(list(oldtree, old.ll))
-        oldtree = tree
-        if(control$trace>1) cat(old.ll, " -> ", newll, "\n") 
-        old.ll = newll
-        loli = parent[1] 
-        
-    }
-    if(control$trace>0) cat(start.ll, " -> ", newll, "\n")
-    list(tree=treeP, logLik=newll, c(eps, iter))
-}
-
 
 
 # bf raus C naeher
