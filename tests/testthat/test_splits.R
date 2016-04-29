a90445350db50069e3de2c0391b1bb92f4511e33
Klaus <klaus.schliep@gmail.com>
2016-04-28 21:33:20 -0400

    cleaned up and added a few tests

--- tests/testthat/test_splits.R
+++ tests/testthat/test_splits.R
@@ -1,22 +1,17 @@
 context("Test conversions")
 
-#  methods(class="splits")
-# addConfidences as.matrix      as.Matrix      as.networx     as.phylo       as.prop.part   cophenetic     c              print         [     
-# as.splits.multiPhylo* as.splits.networx*    as.splits.phylo*      as.splits.prop.part* 
+## generate data
+set.seed(1)
+tree <- rtree(10, FALSE) 
+tree2spl <- as.splits(tree)
+spl2tree <- as.phylo(tree2spl)
+dm <- cophenetic(tree2spl)
+mat <- as.matrix(tree2spl)
+Mat <- as.Matrix(tree2spl)
 
-test_that("test: network <-> igraph", {
+test_that("splits ", {
     ## skip on CRAN
     skip_on_cran()
-    rm(list=ls())
-    
-    ## generate data
-    set.seed(1)
-    tree <- rtree(10, FALSE) 
-    tree2spl <- as.splits(tree)
-    spl2tree <- as.phylo(tree2spl)
-    dm <- cophenetic(tree2spl)
-    mat <- as.matrix(tree2spl)
-    Mat <- as.Matrix(tree2spl)
     
     ## check classes
     expect_is(tree2spl, "splits")
@@ -25,5 +20,4 @@ test_that("test: network <-> igraph", {
     expect_is(mat, "matrix")
     expect_is(Mat, "Matrix")
     expect_equal(spl2tree , tree)
-
 })
--- tests/testthat/test_treedist.R
+++ tests/testthat/test_treedist.R
@@ -1,16 +1,15 @@
 context("treedist")
 
+## generate data
+set.seed(42)
+tree <- rtree(10, FALSE)
+trees <- nni(tree)
+tree1 <- read.tree(text="(t5:1.0,(t4:1.0,t3:1.0):1.0,(t1:1.0,t2:1.0):1.0);")
+tree2 <- read.tree(text="(t4:1.0,(t5:1.0,t3:1.0):1.0,(t1:1.0,t2:1.0):1.0);")
+tree3 <- read.tree(text="(t5:1.0,t4:1.0,t3:1.0,(t1:1.0,t2:1.0):1.0);")
+
 test_that("Robinson-Foulds distance", {
-    ## skip on CRAN
-    skip_on_cran()
-    rm(list=ls())
-    
-    ## generate data
-    tree <- rtree(10, FALSE)
-    trees <- nni(tree)
-    tree1 = read.tree(text="(t5:1.0,(t4:1.0,t3:1.0):1.0,(t1:1.0,t2:1.0):1.0);")
-    tree2 = read.tree(text="(t4:1.0,(t5:1.0,t3:1.0):1.0,(t1:1.0,t2:1.0):1.0);")
-    tree3 = read.tree(text="(t5:1.0,t4:1.0,t3:1.0,(t1:1.0,t2:1.0):1.0);")
+    ## skip_on_cran()
     
     ## check RF.dist and tree dist
     expect_that(RF.dist(tree1, tree1), is_equivalent_to(0))
@@ -24,3 +23,28 @@ test_that("Robinson-Foulds distance", {
     expect_that(treedist(tree1, tree2)[1], is_equivalent_to(2))
     expect_that(treedist(tree1, tree3)[1], is_equivalent_to(1))
 })
+
+test_that("Kuhner-Felsenstein distance (branch score difference)", {
+    ## skip_on_cran()
+    
+    ## check RF.dist and tree dist
+    expect_that(KF.dist(tree1, tree1), is_equivalent_to(0))
+    expect_that(KF.dist(tree1, tree2), is_equivalent_to(sqrt(2)))
+    expect_that(KF.dist(tree1, tree3), is_equivalent_to(1))
+    
+    expect_is(KF.dist(trees),"dist")
+    
+    expect_that(treedist(tree1, tree1)[2], is_equivalent_to(0))
+    expect_that(treedist(tree1, tree2)[2], is_equivalent_to(sqrt(2)))
+    expect_that(treedist(tree1, tree3)[2], is_equivalent_to(1))
+})
+
+test_that("path distance", {
+    ## skip_on_cran()
+    
+    ## check path dist
+    expect_that(path.dist(tree1, tree1), is_equivalent_to(0))
+
+    expect_is(path.dist(trees),"dist")
+})
+
