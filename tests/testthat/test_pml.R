5b21f1c402578d72c38f2cd651faf9dd903c2da4
KlausVigo <klaus.schliep@gmail.com>
2016-07-08 00:20:48 -0700

    added optim.pml tests

--- tests/testthat/test_pml.R
+++ tests/testthat/test_pml.R
@@ -18,7 +18,7 @@ attr(dat, "weight") = weights
 
 dat_Mk <- subset(dat, select = -c(1,342,683, 1024))
 
-bf = c(.1,.2,.3,.4)
+
 Q = c(6:1)
 
 
@@ -63,8 +63,79 @@ test_that("NNI optimisation works properly", {
 })
 
 
+
+test_that("bf optimisation works properly", {
+    skip_on_cran()
+    bf <- c(.1,.2,.3,.4)
+    fit_T <- pml(treeU1, dat, bf=bf)
+    weights <- as.vector(1000 * exp(fit_T$siteLik))
+    dat_tmp <- dat
+    attr(dat_tmp, "weight") <- weights
+    fit0 <- pml(treeU1, dat_tmp)
+    fit.bf <- optim.pml(fit0, optEdge=FALSE, optBf = TRUE,  control = pml.control(epsilon=1e-10, trace=0))
+    expect_equal(logLik(fit.bf), logLik(pml(treeU1, dat_tmp, bf=bf)))
+    expect_equal(bf, fit.bf$bf, tolerance=5e-4)
+})
+
+
+test_that("Q optimisation works properly", {
+    skip_on_cran()
+    Q <- c(6:1)
+    fit_T <- pml(treeU1, dat, Q=Q)
+    weights <- as.vector(1000 * exp(fit_T$siteLik))
+    dat_tmp <- dat
+    attr(dat_tmp, "weight") <- weights
+    fit0 <- pml(treeU1, dat_tmp)
+    fit.Q <- optim.pml(fit0, optEdge=FALSE, optQ = TRUE,  control = pml.control(epsilon=1e-10, trace=0))
+    expect_equal(logLik(fit.Q), logLik(pml(treeU1, dat_tmp, Q=Q)))
+    expect_equal(Q, fit.Q$Q, tolerance=5e-4)
+})
+
+
+test_that("Inv optimisation works properly", {
+    skip_on_cran()
+    inv <- 0.25
+    fit_T <- pml(treeU1, dat, inv=inv)
+    weights <- as.vector(1000 * exp(fit_T$siteLik))
+    dat_tmp <- dat
+    attr(dat_tmp, "weight") <- weights
+    fit0 <- pml(treeU1, dat_tmp)
+    fit.Inv <- optim.pml(fit0, optEdge=FALSE, optInv = TRUE,  control = pml.control(epsilon=1e-10, trace=0))
+    expect_equal(logLik(fit.Inv), logLik(pml(treeU1, dat_tmp, inv=inv)))
+    expect_equal(inv, fit.Inv$inv, tolerance=5e-4)
+})
+
+
+test_that("Gamma optimisation works properly", {
+    skip_on_cran()
+    shape <- 2
+    fit_T <- pml(treeU1, dat, shape=shape, k=4)
+    weights <- as.vector(1000 * exp(fit_T$siteLik))
+    dat_tmp <- dat
+    attr(dat_tmp, "weight") <- weights
+    fit0 <- pml(treeU1, dat_tmp, k=4)
+    fit.Gamma <- optim.pml(fit0, optEdge=FALSE, optGamma = TRUE,  control = pml.control(epsilon=1e-10, trace=0))
+    expect_equal(logLik(fit.Gamma), logLik(pml(treeU1, dat_tmp, shape = shape, k=4)))
+    expect_equal(shape, fit.Gamma$shape, tolerance=5e-4)
+})
+
+
+test_that("rate optimisation works properly", {
+    skip_on_cran()
+    rate <- 2
+    fit_T <- pml(treeU1, dat, rate=rate)
+    weights <- as.vector(1000 * exp(fit_T$siteLik))
+    dat_tmp <- dat
+    attr(dat_tmp, "weight") <- weights
+    fit0 <- pml(treeU1, dat_tmp)
+    fit.rate <- optim.pml(fit0, optEdge=FALSE, optRate = TRUE,  control = pml.control(epsilon=1e-10, trace=0))
+    expect_equal(logLik(fit.rate), logLik(pml(treeU1, dat_tmp, rate=rate)))
+    expect_equal(rate, fit.rate$rate, tolerance=5e-4)
+})
+
 test_that("Mkv model works properly", {
     skip_on_cran()
     expect_equal(logLik(pmlU2.fitted), logLik(pmlU1))
 })
 
+## 31.91%
