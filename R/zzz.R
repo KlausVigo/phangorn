2da28d69663c1a2641886d434b2a51f3348bd351
KlausVigo <klaus.schliep@gmail.com>
2016-10-23 12:25:28 +0200

    add global variable .dnamodels

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,11 @@
 
 .aamodels <- c("WAG", "JTT", "LG", "Dayhoff", "cpREV", "mtmam", "mtArt", "MtZoa", "mtREV24", "VT","RtREV", "HIVw", "HIVb", "FLU","Blosum62","Dayhoff_DCMut","JTT_DCMut")
 
+.dnamodels <- c("JC", "F81", "K80", "HKY", "TrNe", "TrN", 
+  "TPM1", "K81", "TPM1u", "TPM2", "TPM2u", "TPM3", "TPM3u", 
+  "TIM1e", "TIM1", "TIM2e", "TIM2", "TIM3e", "TIM3", "TVMe", 
+  "TVM", "SYM", "GTR")
+
 
 # if g[i] is smaller .gEps inv is increased w[i]
 .gEps <- 1e-30
