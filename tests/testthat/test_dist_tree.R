01e3d9dc59bc8be6f3645c6d1a15e5d31be0a60e
klash <klaus.schliep@gmail.com>
2016-11-07 20:46:20 -0500

    add more tests

---
+++ tests/testthat/test_dist_tree.R
@@ -0,0 +1,22 @@
+context("dist_tree")
+
+data(Laurasiatherian)
+dm <- dist.ml(Laurasiatherian)
+
+
+test_that("check nnls functions", {
+    tree_nj <- NJ(dm)
+    tree_unj <- UNJ(dm)
+    tree_nnls_unj <- nnls.phylo(tree_unj, dm)
+    tree_nnls_nj <- nnls.phylo(tree_nj, dm)
+
+    tree_upgma <- upgma(dm)
+    tree_wpgma <- wpgma(dm)
+    tree_nnls_upgma <- nnls.phylo(tree_upgma, dm, rooted=TRUE)
+    tree_nnls_wpgma <- nnls.phylo(tree_wpgma, dm, rooted=TRUE)
+
+    expect_equal(tree_upgma, tree_nnls_upgma)
+    expect_false(all.equal(tree_wpgma, tree_nnls_wpgma))
+    expect_equal(tree_unj, tree_nnls_unj)
+    expect_false(all.equal(tree_nj, tree_nnls_nj))
+})
