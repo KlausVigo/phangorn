6eecd36f7e8bd5f809a619f7f14cce39fd6bf48f
klash <klaus.schliep@gmail.com>
2017-02-06 17:09:22 -0500

    more tests

--- tests/testthat/test_pmlPart.R
+++ tests/testthat/test_pmlPart.R
@@ -26,7 +26,7 @@ test_that("rate optimisation works properly", {
 
 # nni
 
-# bf + Q
+# Q
 test_that("transition rate optimisation works properly", {
     skip_on_cran()
 
@@ -48,17 +48,120 @@ test_that("transition rate optimisation works properly", {
     # linked parameter
     
     sp <- pmlPart(edge + Q ~ ., fit0, weight=W)    
-    expect_equal(logLik(sp), logLik(fit1)*3, tolerance=5e-4 )
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
     expect_equal(Q, sp$fits[[1]]$Q, tolerance=5e-4)
     
     # unlinked parameter
     # TODO more complicated models
     # weights0 <- 1000*exp(fit0$site)
     sp <- pmlPart( ~ Q, fit0, weight=W)   
-    expect_equal(logLik(sp), logLik(fit1)*3, tolerance=5e-4 )
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
     expect_equal(Q, sp$fits[[1]]$Q, tolerance=5e-4)
 })
 
+
+# bf
+test_that("base frequency optimisation works properly", {
+    skip_on_cran()
+    
+    bf <- (1:4)/10 
+    
+    fit0 <- pml(tree, X, k=4)
+    fit1 <- pml(tree, X, k=4, bf=bf)
+    weights1 <- 1000*exp(fit1$site) 
+    Y <- X 
+    attr(Y, "weight") <- weights1
+    fit1 <- pml(tree, Y, k=4, bf=bf)
+    
+    weights0 <- weights1
+    weights2 <- weights1
+    
+    W = cbind(weights0, weights1, weights2) 
+    colnames(W) = c("g1", "g2", "g3")
+    
+    # linked parameter
+    
+    sp <- pmlPart(edge + bf ~ ., fit0, weight=W)    
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
+    expect_equal(bf, sp$fits[[1]]$bf, tolerance=5e-4)
+    
+    # unlinked parameter
+    # TODO more complicated models
+    # weights0 <- 1000*exp(fit0$site)
+    sp <- pmlPart( ~ bf, fit0, weight=W)   
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
+    expect_equal(bf, sp$fits[[1]]$bf, tolerance=5e-4)
+})
+
+# Gamma
+test_that("shape parameter optimisation works properly", {
+    skip_on_cran()
+    
+    shape=2
+    
+    fit0 <- pml(tree, X, k=4)
+    fit1 <- pml(tree, X, k=4, shape=shape)
+    weights1 <- 1000*exp(fit1$site) 
+    Y <- X 
+    attr(Y, "weight") <- weights1
+    fit1 <- pml(tree, Y, k=4, shape=shape)
+    
+    weights0 <- weights1
+    weights2 <- weights1
+    
+    W = cbind(weights0, weights1, weights2) 
+    colnames(W) = c("g1", "g2", "g3")
+    
+    # linked parameter
+  
+    sp <- pmlPart(edge + shape ~ ., fit0, weight=W)    
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
+    expect_equal(shape, sp$fits[[1]]$shape, tolerance=5e-3)
+    
+    # unlinked parameter
+    # TODO more complicated models
+    # weights0 <- 1000*exp(fit0$site)
+    sp <- pmlPart( ~ shape, fit0, weight=W)   
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
+    expect_equal(shape, sp$fits[[1]]$shape, tolerance=5e-4)
+})
+
+# Invariant sites
+test_that("Invariant sites optimisation works properly", {
+    skip_on_cran()
+    
+    inv=.2
+    
+    fit0 <- pml(tree, X, k=4)
+    fit1 <- pml(tree, X, k=4, inv=inv)
+    weights1 <- 1000*exp(fit1$site) 
+    Y <- X 
+    attr(Y, "weight") <- weights1
+    fit1 <- pml(tree, Y, k=4, inv=inv)
+    
+    weights0 <- weights1
+    weights2 <- weights1
+    
+    W = cbind(weights0, weights1, weights2) 
+    colnames(W) = c("g1", "g2", "g3")
+    
+    # linked parameter
+    
+    sp <- pmlPart(edge + inv ~ ., fit0, weight=W)    
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
+    expect_equal(inv, sp$fits[[1]]$inv, tolerance=5e-5)
+    
+    # unlinked parameter
+    # TODO more complicated models
+    # weights0 <- 1000*exp(fit0$site)
+    sp <- pmlPart( ~ inv, fit0, weight=W)   
+    expect_equal(logLik(sp)[1], logLik(fit1)[1]*3, tolerance=5e-4 )
+    expect_equal(inv, sp$fits[[1]]$inv, tolerance=5e-5)
+})
+
+
 # model
 
-# rate
+
+
+
