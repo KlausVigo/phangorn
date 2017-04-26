473de0810bba523c1f5e8dd0d7944cf1ca85f73c
KlausVigo <klaus.schliep@gmail.com>
2017-04-26 00:51:27 -0400

    added (slow) mast version for unrooted trees

--- R/mast.R
+++ R/mast.R
@@ -2,13 +2,15 @@
 #' 
 #' \code{mast} computes the maximum agreement subtree (MAST). 
 #' 
-#' The code is derived from exames in Valverde (2009), for the original code see 
-#' \url{http://www.cs.upc.edu/~valiente/comput-biol/}. 
+#' The code is derived from the code example in Valverde (2009), 
+#' for the original code see \url{http://www.cs.upc.edu/~valiente/comput-biol/}. 
+#' The version for the unrooted trees is much slower.
 #' 
 #' @param x a tree, i.e. an object of class \code{phylo}.
 #' @param y a tree, i.e. an object of class \code{phylo}.
-#' @param tree a logicical ,if TRUE returns a tree other wise the tip labels 
+#' @param tree a logicical, if TRUE returns a tree other wise the tip labels 
 #' of the the maximum agreement subtree.
+#' @param rooted logical if TRUE treats trees as rooted otherwise unrooted.
 #' @return \code{mast} returns a vector of the tip labels in the MAST.
 #' @author Klaus Schliep \email{klaus.schliep@@gmail.com} 
 #' @seealso \code{\link{SPR.dist}}
@@ -24,32 +26,65 @@
 #' 
 #' @rdname mast
 #' @export
-mast <- function (x, y, tree=TRUE) {
-    # make x and y have same taxa set
+mast <- function (x, y, tree=TRUE, rooted=TRUE) {
+
+    if(!is.rooted(x) | !is.rooted(y)){
+        rooted <- FALSE
+    }
+    
     shared_tips <- intersect(x$tip.label, y$tip.label)
     if(length(shared_tips) < length(x$tip.label)) 
         x <- drop.tip(x, setdiff(x$tip.label, shared_tips))
     if(length(shared_tips) < length(y$tip.label)) 
         y <- drop.tip(y, setdiff(y$tip.label, shared_tips))
-    
-    is.ancestor <- function(anc, des, pvec) {
-        if (anc==des) return(TRUE)
-        return(anc %in% pvec)
-    }
-    
+
     # make order of labels the same
     y <- .compressTipLabel(c(y), x$tip.label)[[1]]
     x <- reorder(x, "postorder")
+
+    if(rooted)res <- mast.fit(x,y)
+    else {
+        x <- reorder(x, "postorder")
+        bipart_x <- bipartCPP(x$edge, Ntip(x))[[2]]
+        x <- root(x, bipart_x, resolve.root = TRUE)
+        x <- reorder(x, "postorder")
+
+        y <- unroot(y)
+        y <- reorder(y, "postorder")
+
+        res <- NULL
+        
+        bipart_y <- bipartCPP(y$edge, Ntip(y))
+        #unique(y$edge[,1])
+        for(i in 2:length(bipart_y)){
+            y <- root(y, bipart_y[[i]], resolve=TRUE)
+            tmp <- mast.fit(x,y)
+            if(length(tmp) > length(res)) res <- tmp
+        }
+    }
+    if(tree) res <- drop.tip(x, setdiff(x$tip.label, res))
+    res
+}
+
+
+is.ancestor <- function(anc, des, pvec) {
+    if (anc==des) return(TRUE)
+    return(anc %in% pvec)
+}
+
+
+mast.fit <- function (x, y) {
     y <- reorder(y, "postorder")
     
     po1 <- c(x$edge[,2], x$edge[nrow(x$edge),1])
     po2 <- c(y$edge[,2], y$edge[nrow(y$edge),1])
     
     nTips <- length(x$tip.label)
-    
+# vielleicht ausserhalb    
     p_vec_1 <- Ancestors(x, 1L:max(x$edge))
     p_vec_2 <- Ancestors(y, 1L:max(y$edge))
     
+# vielleicht ausserhalb    
     CH1 <- allChildren(x)
     CH2 <- allChildren(y)
     
@@ -92,6 +127,5 @@ mast <- function (x, y, tree=TRUE) {
         }
     }
     res <- unlist(m[[length(x$tip.label)+1L,length(y$tip.label)+1L]])
-    if(tree) res <- drop.tip(x, setdiff(x$tip.label, res))
-    res
 }
+
