6e10b17b8135353ac0b907b9c2742e1f2935f42f
KlausVigo <klaus.schliep@gmail.com>
2017-09-26 07:01:31 -0400

    add test

--- tests/testthat/test_ancestral.R
+++ tests/testthat/test_ancestral.R
@@ -9,15 +9,18 @@ dat <- matrix( c("a", "a",
                dimnames = list(c("t1", "t2", "t3", "t4"), NULL))
 dna <- phyDat(dat)
 fit <- pml(tree, dna)
-anc.ml <- ancestral.pml(fit, type = "ml")
+
 
 
 # dna tests differs from other data types as it may returns ambiguous data
 test_that("test ancestral dna", {
+    test.ml1 <- ancestral.pml(fit, type = "ml")
+    test.ml2 <- ancestral.pml(fit, type = "ml", return = "phyDat")
     test1 <- ancestral.pars(tree, dna, "MPR", return = "prob")
     test2 <- ancestral.pars(tree, dna, "MPR", return = "phyDat")
     test3 <- ancestral.pars(tree, dna, "ACCTRAN", return = "prob")
     test4 <- ancestral.pars(tree, dna, "ACCTRAN", return = "phyDat")
     expect_equal(as.character(test2), as.character(test4))
+    expect_equal(as.character(test.ml2), as.character(test2))
 })
 
