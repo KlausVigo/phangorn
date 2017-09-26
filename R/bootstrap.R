76612a00315d6975ab6ad4af371fe6ac856bf143
KlausVigo <klaus.schliep@gmail.com>
2017-09-26 06:07:36 -0400

    apply goodpractice

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -59,7 +59,8 @@
 #' tree <- NJ(dm)
 #' # NJ
 #' set.seed(123)
-#' NJtrees <- bootstrap.phyDat(Laurasiatherian, FUN=function(x)NJ(dist.logDet(x)), bs=100)
+#' NJtrees <- bootstrap.phyDat(Laurasiatherian, 
+#'      FUN=function(x)NJ(dist.logDet(x)), bs=100)
 #' treeNJ <- plotBS(tree, NJtrees, "phylogram")
 #' 
 #' # Maximum likelihood
@@ -83,30 +84,31 @@
 #' 
 #' @rdname bootstrap.pml
 #' @export
-bootstrap.pml <- function (x, bs = 100, trees = TRUE, multicore=FALSE, mc.cores = NULL, ...) 
+bootstrap.pml <- function (x, bs = 100, trees = TRUE, multicore=FALSE, 
+                           mc.cores = NULL, ...) 
 {
     if(multicore && is.null(mc.cores)){
         mc.cores <- detectCores()
     }
     
-    data = x$data
-    weight = attr(data, "weight")
-    v = rep(1:length(weight), weight)
-    BS = vector("list", bs)
-    for (i in 1:bs) BS[[i]] = tabulate(sample(v, replace = TRUE), 
+    data <- x$data
+    weight <- attr(data, "weight")
+    v <- rep(1:length(weight), weight)
+    BS <- vector("list", bs)
+    for (i in 1:bs) BS[[i]] <- tabulate(sample(v, replace = TRUE), 
                                        length(weight))
     pmlPar <- function(weights, fit, trees = TRUE, ...) {
-        data = fit$data
+        data <- fit$data
         ind <- which(weights > 0)
         data <- getRows(data, ind)
         attr(data, "weight") <- weights[ind]
-        fit = update(fit, data = data)
-        fit = optim.pml(fit, ...)
+        fit <- update(fit, data = data)
+        fit <- optim.pml(fit, ...)
         if (trees) {
-            tree = fit$tree
+            tree <- fit$tree
             return(tree)
         }
-        attr(fit, "data") = NULL
+        attr(fit, "data") <- NULL
         fit
     }
     eval.success <- FALSE
@@ -116,8 +118,8 @@ bootstrap.pml <- function (x, bs = 100, trees = TRUE, multicore=FALSE, mc.cores 
     }
     if (!eval.success) res <- lapply(BS, pmlPar, x, trees = trees, ...)
     if (trees) {
-        class(res) = "multiPhylo"
-        res = .compressTipLabel(res) # save memory
+        class(res) <- "multiPhylo"
+        res <- .compressTipLabel(res) # save memory
     }
     res
 }
@@ -125,19 +127,20 @@ bootstrap.pml <- function (x, bs = 100, trees = TRUE, multicore=FALSE, mc.cores 
 
 #' @rdname bootstrap.pml
 #' @export
-bootstrap.phyDat <- function (x, FUN, bs = 100, multicore=FALSE, mc.cores = NULL, jumble=TRUE, ...) 
+bootstrap.phyDat <- function(x, FUN, bs = 100, multicore=FALSE, mc.cores = NULL, 
+            jumble=TRUE, ...) 
 {
     if(multicore && is.null(mc.cores)){
         mc.cores <- detectCores()
     }
-    weight = attr(x, "weight")
-    v = rep(1:length(weight), weight)
-    BS = vector("list", bs)
+    weight <- attr(x, "weight")
+    v <- rep(1:length(weight), weight)
+    BS <- vector("list", bs)
     for(i in 1:bs)BS[[i]]=tabulate(sample(v, replace=TRUE),length(weight)) 
     if(jumble){
-        J = vector("list", bs)
-        l = length(x)
-        for(i in 1:bs) J[[i]] = list(BS[[i]], sample(l))
+        J <- vector("list", bs)
+        l <- length(x)
+        for(i in 1:bs) J[[i]] <- list(BS[[i]], sample(l))
     }
     fitPar <- function(weights, data, ...){     
         ind <- which(weights > 0)
@@ -162,7 +165,7 @@ bootstrap.phyDat <- function (x, FUN, bs = 100, multicore=FALSE, mc.cores = NULL
     } 
     if(class(res[[1]]) == "phylo"){
         class(res) <- "multiPhylo"   
-        res = .compressTipLabel(res) # save memory
+        res <- .compressTipLabel(res) # save memory
     }
     res 
 }
@@ -176,8 +179,8 @@ matchEdges <- function(tree1, tree2){
         if(x[1]==1)return(x)
         else return(y[-x])
     } 
-    bp1[] = lapply(bp1, fn, 1:l)
-    bp2[] = lapply(bp2, fn, 1:l)
+    bp1[] <- lapply(bp1, fn, 1:l)
+    bp2[] <- lapply(bp2, fn, 1:l)
     match(bp1, bp2)
 }
 
@@ -202,8 +205,8 @@ plotBS <- function (tree, BStrees, type = "unrooted", bs.col = "black",
                               "unrooted", "radial"))
     if (type == "phylogram" | type == "cladogram") {
         if (!is.rooted(tree) & !is.null(tree$edge.length)) 
-            tree2 = midpoint(tree)
-        else tree2=tree
+            tree2 <- midpoint(tree)
+        else tree2 <- tree
         plot(tree2, type = type, ...)
     }
     else plot(tree, type = type, ...)
@@ -214,9 +217,9 @@ plotBS <- function (tree, BStrees, type = "unrooted", bs.col = "black",
 #        if(any(unlist(lapply(BStrees, is.rooted)))){
 #            BStrees <- lapply(BStrees, unroot)   
 #        }
-        x = prop.clades(tree, BStrees)
-        x = round((x/length(BStrees)) * 100)
-        tree$node.label = x
+        x <- prop.clades(tree, BStrees)
+        x <- round((x/length(BStrees)) * 100)
+        tree$node.label <- x
     }
     else{
         if(is.null(tree$node.label))stop("You need to supply BStrees or tree needs 
@@ -224,28 +227,28 @@ plotBS <- function (tree, BStrees, type = "unrooted", bs.col = "black",
         x <- tree$node.label
     }
     
-    label = c(rep(0, length(tree$tip.label)), x)
+    label <- c(rep(0, length(tree$tip.label)), x)
     ind <- get("last_plot.phylo", envir = .PlotPhyloEnv)$edge[, 
                                                               2]
     if (type == "phylogram" | type == "cladogram") {
-        root = getRoot(tree)
-        label = c(rep(0, length(tree$tip.label)), x)
-        label[root] = 0
-        ind2 = matchEdges(tree2, tree)
-        label = label[ind2]
-        ind = which(label > p)
+        root <- getRoot(tree)
+        label <- c(rep(0, length(tree$tip.label)), x)
+        label[root] <- 0
+        ind2 <- matchEdges(tree2, tree)
+        label <- label[ind2]
+        ind <- which(label > p)
         #        browser()
         if (is.null(bs.adj)) 
-            bs.adj = c(1, 1)
-        if(length(ind)>0)nodelabels(text = label[ind], node = ind, frame = frame, 
-                                    col = bs.col, adj = bs.adj, ...)
+            bs.adj <- c(1, 1)
+        if(length(ind)>0)nodelabels(text = label[ind], node = ind, 
+                            frame = frame, col = bs.col, adj = bs.adj, ...)
     }
     else {
         if (is.null(bs.adj)) 
-            bs.adj = c(0.5, 0.5)
-        ind2 = which(label[ind]>p)
-        if(length(ind2>0))edgelabels(label[ind][ind2],ind2, frame = frame, col = bs.col, 
-                                     adj = bs.adj, ...)
+            bs.adj <- c(0.5, 0.5)
+        ind2 <- which(label[ind]>p)
+        if(length(ind2>0))edgelabels(label[ind][ind2],ind2, frame = frame, 
+                col = bs.col, adj = bs.adj, ...)
     }
     invisible(tree)
 }
@@ -364,7 +367,7 @@ cladeMatrix <- function(x, rooted=FALSE){
         ppi <- prop.part(x[[i]])  
         if(!rooted)ppi <- oneWise(ppi)
         indi <- sort(fmatch(ppi, pp))
-        ivec[from[i]:to[i]] = indi
+        ivec[from[i]:to[i]] <- indi
     }
     X <- sparseMatrix(i=ivec, p=pvec, dims=c(length(pp),l))
     list(X=X, prop.part=pp)
