0841af640b26492221785fdce5399a0c8f7fd2a1
klash <klaus.schliep@gmail.com>
2018-01-11 14:58:32 -0500

    reorganise code

--- R/treeManipulation.R
+++ R/treeManipulation.R
@@ -455,6 +455,43 @@ add.tip <- function(phy, n, edgeLength=NULL, tip=""){
 
 
 
+# maybe into treeManipulation
+addAllTips <- function(tree, mapping){
+    tree2 <- tree
+    uni <- unique(mapping[,2])
+    for(i in uni)
+        tree2 <- addTips2Tree(tree2, mapping[mapping[,2]==i,1],i)
+    tree2
+}
+
+
+
+addTips2Tree <- function (tree, tips, where){
+    nTips <- length(tree$tip.label)
+    edge <- tree$edge
+    
+    where <- match(where, tree$tip.label)
+    
+    ind <- match(where, edge[,2])
+    nt <- as.integer(length(tips))
+    edge[edge>nTips] <- edge[edge>nTips]+nt
+    m <- max(edge)
+    edge[ind,2] <- m+1L    
+    
+    newEdge <- cbind(m+1L,c(where,(nTips+1L):(nTips+nt)))
+    edge <- rbind(newEdge, edge)
+    storage.mode(edge) <- "integer"
+    if(!is.null(tree$edge.length)) tree$edge.length <- c(rep(0, nrow(newEdge)),
+                                                         tree$edge.length)
+    tree$edge <- edge
+    tree$Nnode <- tree$Nnode + 1L
+    tree$tip.label <- c(tree$tip.label, tips)
+    attr(tree, "order") <- NULL
+    tree <- reorder(tree, "postorder")
+    tree
+}    
+
+
 
 #' Compute all trees topologies.
 #' 
