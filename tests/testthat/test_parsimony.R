cd8bc1c3811ecd6a3e7eb49ca93cc31092c7a61f
klash <klaus.schliep@gmail.com>
2018-03-22 21:30:47 -0400

    clean up

--- tests/testthat/test_parsimony.R
+++ tests/testthat/test_parsimony.R
@@ -6,10 +6,9 @@ all_trees <- allTrees(8, tip.label = names(yeast))
 tree1 <- read.tree(text = "((t1,t2),t3,t4);")
 tree2 <- read.tree(text = "((t1,t3),t2,t4);")
 trees <- .compressTipLabel(c(tree1, tree2))
-dat <- phyDat(c(t1="a", t2="a",t3="t",t4="t"), type="USER", levels=c("a","c","g","t"))
-#tr_acctran = acctran(tree1, dat)
-#tr_ratchet = pratchet(dat, trace=0)
-#bab(dat)
+dat <- phyDat(c(t1="a", t2="a",t3="t",t4="t"), type="USER", 
+              levels=c("a","c","g","t"))
+
 test_that("parsimony works properly", {
 ##    skip_on_cran()
     expect_that(fitch(tree1, dat), equals(1))
@@ -24,7 +23,7 @@ test_that("bab works properly", {
     skip_on_cran()
 #    all_trees <- allTrees(8, tip.label = names(yeast))
     all_pars <- fitch(all_trees, yeast)
-    bab_tree <- bab(yeast)
+    bab_tree <- bab(yeast, trace=0)
     expect_equal(min(all_pars), fitch(bab_tree, yeast))
 })
 
@@ -32,10 +31,11 @@ test_that("rearrangements works properly", {
     skip_on_cran()
     tree <- all_trees[[1]]
     start <- fitch(tree, yeast)
-    bab_tree <- bab(yeast)
+    bab_tree <- bab(yeast, trace=0)
     best <- fitch(bab_tree, yeast)
-    best_fitch <- optim.parsimony(tree, yeast, rearrangements = "NNI")
-    best_sankoff <- optim.parsimony(tree, yeast, method="sankoff", rearrangements = "NNI")
+    best_fitch <- optim.parsimony(tree, yeast, rearrangements = "NNI", trace=0)
+    best_sankoff <- optim.parsimony(tree, yeast, method="sankoff", 
+                                    rearrangements = "NNI", trace=0)
     expect_equal(attr(best_fitch, "pscore"), attr(best_sankoff, "pscore"))
 })
 
