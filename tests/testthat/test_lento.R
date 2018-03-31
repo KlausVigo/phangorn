0f65d914b8c2a1c608de56a0f4d641cb299c9c40
klash <klaus.schliep@gmail.com>
2018-03-31 19:10:45 -0400

    little improvement

--- tests/testthat/test_lento.R
+++ tests/testthat/test_lento.R
@@ -4,10 +4,10 @@ context("lento plot")
 
 # Visual tests ------------------------------------------------------------
 test_that("visual appearance", {
+    skip_on_cran()
     data(yeast)
     yeast.ry <- acgt2ry(yeast)
     splits.h <- h2st(yeast.ry)
     lentoPlot <- function() lento(splits.h)
-    skip_on_cran()
     vdiffr::expect_doppelganger("lento plot", lentoPlot)
 })
