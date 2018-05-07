5a0a6009e3926308baaecca5b3b4bf96e38ee299
KlausVigo <klaus.schliep@gmail.com>
2018-05-07 17:28:35 -0400

    reorganize code

--- R/F3X4.R
+++ R/F3X4.R
@@ -1,18 +1,7 @@
-# environment variables
-CodonAlphabet <- c("aaa", "aac", "aag", "aat", "aca", "acc", "acg", "act", 
-                  "aga", "agc", "agg", "agt", "ata", "atc", "atg", "att", 
-                  "caa", "cac", "cag", "cat", "cca", "ccc", "ccg", "cct", 
-                  "cga", "cgc", "cgg", "cgt", "cta", "ctc", "ctg", "ctt", 
-                  "gaa", "gac", "gag", "gat", "gca", "gcc", "gcg", "gct", 
-                  "gga", "ggc", "ggg", "ggt", "gta", "gtc", "gtg", "gtt", 
-                  "tac", "tat", "tca", "tcc", "tcg", "tct", "tgc", "tgg", 
-                  "tgt", "tta", "ttc", "ttg", "ttt") 
-# order is defined as per phangorn -> phyDat.R -> line 324
-nucleotideAlphabet <- c("a", "c", "g", "t")
 
 # estimate codon frequencies based on the F3X4 method
-getF3X4codon_frequencies <- function(codon_data, codon_alphabet = CodonAlphabet,
-                                     nucleotide_alphabet = nucleotideAlphabet) {
+getF3X4codon_frequencies <- function(codon_data, codon_alphabet=.CodonAlphabet,
+                                     nucleotide_alphabet=.nucleotideAlphabet) {
     epirical_codon_frequenices <- baseFreq(codon_data)
     # get the position-wise nucleotide frequencies
     nuc_freq_by_pos <- matrix(0, nrow=length(nucleotide_alphabet), ncol=3)
@@ -22,7 +11,7 @@ getF3X4codon_frequencies <- function(codon_data, codon_alphabet = CodonAlphabet,
         for (j in 1:3) {
             nuc <- substr(codon,j,j)
             nuc_index <- which(nucleotide_alphabet == nuc)
-            nuc_freq_by_pos[nuc_index,j] = nuc_freq_by_pos[nuc_index,j] + codon_freq
+            nuc_freq_by_pos[nuc_index,j] <- nuc_freq_by_pos[nuc_index,j] + codon_freq
         }
     }
     # construct the codon frequencies according to the F3X4 method
@@ -38,8 +27,8 @@ getF3X4codon_frequencies <- function(codon_data, codon_alphabet = CodonAlphabet,
     return(codon_frequencies)
 }
 
-
-# return M
+# compute frequencies for codon positions
+# TODO: using baseFreq codon should save memory instead of codon2dna
 bf_by_codon <- function(x){
     y <- codon2dna(x)
     ny <- sum(attr(y, "weight"))
@@ -50,8 +39,9 @@ bf_by_codon <- function(x){
     M
 }
 
-# return
-F3x4_freq <- function(M, CodonAlphabet, nucleotideAlphabet){
+# return frequencies for all 61 states
+F3x4_freq <- function(M, CodonAlphabet=.CodonAlphabet, 
+                      nucleotideAlphabet=.nucleotideAlphabet){
     pos <- CodonAlphabet %>% strsplit("") %>% unlist %>% 
         match(nucleotideAlphabet) %>% matrix(ncol=3, byrow = TRUE) 
     M[pos[,1],1] * M[pos[,2],2] * M[pos[,3],3]
--- R/zzz.R
+++ R/zzz.R
@@ -9,6 +9,18 @@
   "TIM1e", "TIM1", "TIM2e", "TIM2", "TIM3e", "TIM3", "TVMe", 
   "TVM", "SYM", "GTR")
 
+# environment variables
+.CodonAlphabet <- c("aaa", "aac", "aag", "aat", "aca", "acc", "acg", "act", 
+                    "aga", "agc", "agg", "agt", "ata", "atc", "atg", "att", 
+                    "caa", "cac", "cag", "cat", "cca", "ccc", "ccg", "cct", 
+                    "cga", "cgc", "cgg", "cgt", "cta", "ctc", "ctg", "ctt", 
+                    "gaa", "gac", "gag", "gat", "gca", "gcc", "gcg", "gct", 
+                    "gga", "ggc", "ggg", "ggt", "gta", "gtc", "gtg", "gtt", 
+                    "tac", "tat", "tca", "tcc", "tcg", "tct", "tgc", "tgg", 
+                    "tgt", "tta", "ttc", "ttg", "ttt") 
+
+.nucleotideAlphabet <- c("a", "c", "g", "t")
+
 
 # if rate g[i] is smaller than .gEps invariant site is increased by w[i]
 .gEps <- 1e-12
