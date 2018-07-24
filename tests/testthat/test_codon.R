7377f4c6af08442030d3f0aaf4e58f38a6850880
klash <klaus.schliep@gmail.com>
2018-07-24 19:11:40 -0400

    some more tests

--- tests/testthat/test_codon.R
+++ tests/testthat/test_codon.R
@@ -3,9 +3,24 @@ context("codon")
 
 tree <- read.tree(text = "(((t1:.1,t2:.1):.1,t3:.2):.1,(t4:.1,t5:.1):.2);")
 set.seed(42)
-dat_dna <- simSeq(tree, l=300)
-dat_codon <- dna2codon(dat_dna)
+dat_1 <- simSeq(tree, l=1000, type = "CODON")
+dat_2 <- simSeq(tree, l=1000, type = "CODON", dnds=.5)
+dat_3 <- simSeq(tree, l=1000, type = "CODON", tstv=2)
+dat_4 <- simSeq(tree, l=1000, type = "CODON", dnds=2, tstv=2)
 
-fit_F1x4 <- pml(tree, dat_codon, bf="F1x4")
-fit_F3x4 <- pml(tree, dat_codon, bf="F3x4")
-fit_GY <- pml(tree, dat_codon, bf="empirical")
+
+
+fit_F1x4 <- pml(tree, dat_4, bf="F1x4")
+fit_F3x4 <- pml(tree, dat_4, bf="F3x4")
+fit_GY <- pml(tree, dat_4, bf="empirical")
+
+
+test_that("edge length optimisation works properly", {
+    skip_on_cran()
+    
+    fit_GY_opt <- optim.pml(fit_GY, model="codon1")
+    expect_gt(fit_GY_opt$dnds, 1)
+    expect_gt(fit_GY_opt$tstv, 1)
+})
+
+
