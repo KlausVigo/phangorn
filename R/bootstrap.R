4e9be34785a3ba71c6f544dd9c25cc4db7027845
KlausVigo <klaus.schliep@gmail.com>
2018-08-18 16:22:56 -0400

    stylish R code

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,15 +1,15 @@
 #' Bootstrap
-#' 
+#'
 #' \code{bootstrap.pml} performs (non-parametric) bootstrap analysis and
 #' \code{bootstrap.phyDat} produces a list of bootstrapped data sets.
 #' \code{plotBS} plots a phylogenetic tree with the with the bootstrap values
 #' assigned to the (internal) edges.
-#' 
+#'
 #' It is possible that the bootstrap is performed in parallel, with help of the
 #' multicore package. Unfortunately the multicore package does not work under
 #' windows or with GUI interfaces ("aqua" on a mac). However it will speed up
 #' nicely from the command line ("X11").
-#' 
+#'
 #' @param x an object of class \code{pml} or \code{phyDat}.
 #' @param bs number of bootstrap samples.
 #' @param trees return trees only (default) or whole \code{pml} objects.
@@ -44,32 +44,32 @@
 #' \code{\link{SOWH.test}} for parametric bootstrap
 #' @references Felsenstein J. (1985) Confidence limits on phylogenies. An
 #' approach using the bootstrap. \emph{Evolution} \bold{39}, 783--791
-#' 
+#'
 #' Penny D. and Hendy M.D. (1985) Testing methods evolutionary tree
 #' construction. \emph{Cladistics} \bold{1}, 266--278
-#' 
+#'
 #' Penny D. and Hendy M.D. (1986) Estimating the reliability of evolutionary
 #' trees. \emph{Molecular Biology and Evolution} \bold{3}, 403--417
 #' @keywords cluster
 #' @examples
-#' 
+#'
 #' \dontrun{
 #' data(Laurasiatherian)
 #' dm <- dist.logDet(Laurasiatherian)
 #' tree <- NJ(dm)
 #' # NJ
 #' set.seed(123)
-#' NJtrees <- bootstrap.phyDat(Laurasiatherian, 
+#' NJtrees <- bootstrap.phyDat(Laurasiatherian,
 #'      FUN=function(x)NJ(dist.logDet(x)), bs=100)
 #' treeNJ <- plotBS(tree, NJtrees, "phylogram")
-#' 
+#'
 #' # Maximum likelihood
 #' fit <- pml(tree, Laurasiatherian)
 #' fit <- optim.pml(fit, rearrangements="NNI")
 #' set.seed(123)
 #' bs <- bootstrap.pml(fit, bs=100, optNni=TRUE)
 #' treeBS <- plotBS(fit$tree,bs)
-#' 
+#'
 #' # Maximum parsimony
 #' treeMP <- pratchet(Laurasiatherian)
 #' treeMP <- acctran(treeMP, Laurasiatherian)
@@ -77,195 +77,222 @@
 #' BStrees <- bootstrap.phyDat(Laurasiatherian, pratchet, bs = 100)
 #' treeMP <- plotBS(treeMP, BStrees, "phylogram")
 #' add.scale.bar()
-#' 
+#'
 #' # export tree with bootstrap values as node labels
 #' # write.tree(treeBS)
 #' }
-#' 
+#'
 #' @rdname bootstrap.pml
 #' @export
-bootstrap.pml <- function (x, bs = 100, trees = TRUE, multicore=FALSE, 
-                           mc.cores = NULL, ...) 
-{
-    if(multicore && is.null(mc.cores)){
-        mc.cores <- detectCores()
-    }
-    
-    data <- x$data
-    weight <- attr(data, "weight")
-    v <- rep(seq_along(weight), weight)
-    BS <- vector("list", bs)
-    for (i in 1:bs) BS[[i]] <- tabulate(sample(v, replace = TRUE), 
-                                       length(weight))
-    pmlPar <- function(weights, fit, trees = TRUE, ...) {
-        data <- fit$data
-        ind <- which(weights > 0)
-        data <- getRows(data, ind)
-        attr(data, "weight") <- weights[ind]
-        fit <- update(fit, data = data)
-        fit <- optim.pml(fit, ...)
-        if (trees) {
-            tree <- fit$tree
-            return(tree)
-        }
-        attr(fit, "data") <- NULL
-        fit
-    }
-    eval.success <- FALSE
-    if (!eval.success & multicore) {
-        res <- mclapply(BS, pmlPar, x, trees = trees, ..., mc.cores = mc.cores)
-        eval.success <- TRUE
-    }
-    if (!eval.success) res <- lapply(BS, pmlPar, x, trees = trees, ...)
+bootstrap.pml <- function(x, bs = 100, trees = TRUE, multicore = FALSE,
+                          mc.cores = NULL, ...) {
+  if (multicore && is.null(mc.cores)) {
+    mc.cores <- detectCores()
+  }
+
+  data <- x$data
+  weight <- attr(data, "weight")
+  v <- rep(seq_along(weight), weight)
+  BS <- vector("list", bs)
+  for (i in 1:bs) BS[[i]] <- tabulate(
+      sample(v, replace = TRUE),
+      length(weight)
+    )
+  pmlPar <- function(weights, fit, trees = TRUE, ...) {
+    data <- fit$data
+    ind <- which(weights > 0)
+    data <- getRows(data, ind)
+    attr(data, "weight") <- weights[ind]
+    fit <- update(fit, data = data)
+    fit <- optim.pml(fit, ...)
     if (trees) {
-        class(res) <- "multiPhylo"
-        res <- .compressTipLabel(res) # save memory
+      tree <- fit$tree
+      return(tree)
     }
-    res
+    attr(fit, "data") <- NULL
+    fit
+  }
+  eval.success <- FALSE
+  if (!eval.success & multicore) {
+    res <- mclapply(BS, pmlPar, x, trees = trees, ..., mc.cores = mc.cores)
+    eval.success <- TRUE
+  }
+  if (!eval.success) res <- lapply(BS, pmlPar, x, trees = trees, ...)
+  if (trees) {
+    class(res) <- "multiPhylo"
+    res <- .compressTipLabel(res) # save memory
+  }
+  res
 }
 
 
 #' @rdname bootstrap.pml
 #' @export
-bootstrap.phyDat <- function(x, FUN, bs = 100, multicore=FALSE, mc.cores = NULL, 
-            jumble=TRUE, ...) 
-{
-    if(multicore && is.null(mc.cores)){
-        mc.cores <- detectCores()
+bootstrap.phyDat <- function(x, FUN, bs = 100, multicore = FALSE,
+                             mc.cores = NULL, jumble = TRUE, ...) {
+  if (multicore && is.null(mc.cores)) {
+    mc.cores <- detectCores()
+  }
+  weight <- attr(x, "weight")
+  v <- rep(seq_along(weight), weight)
+  BS <- vector("list", bs)
+  for (i in 1:bs) BS[[i]] <- tabulate(sample(v, replace = TRUE), length(weight))
+  if (jumble) {
+    J <- vector("list", bs)
+    l <- length(x)
+    for (i in 1:bs) J[[i]] <- list(BS[[i]], sample(l))
+  }
+  fitPar <- function(weights, data, ...) {
+    ind <- which(weights > 0)
+    data <- getRows(data, ind)
+    attr(data, "weight") <- weights[ind]
+    FUN(data, ...)
+  }
+  fitParJumble <- function(J, data, ...) {
+    ind <- which(J[[1]] > 0)
+    data <- getRows(data, ind)
+    attr(data, "weight") <- J[[1]][ind]
+    data <- subset(data, J[[2]])
+    FUN(data, ...)
+  }
+  if (multicore) {
+    if (jumble) {
+      res <- mclapply(J, fitParJumble, x, ..., mc.cores = mc.cores)
+    } else {
+      res <- mclapply(BS, fitPar, x, ..., mc.cores = mc.cores)
     }
-    weight <- attr(x, "weight")
-    v <- rep(seq_along(weight), weight)
-    BS <- vector("list", bs)
-    for(i in 1:bs)BS[[i]] <- tabulate(sample(v, replace=TRUE),length(weight)) 
-    if(jumble){
-        J <- vector("list", bs)
-        l <- length(x)
-        for(i in 1:bs) J[[i]] <- list(BS[[i]], sample(l))
+  }
+  else {
+    if (jumble) {
+      res <- lapply(J, fitParJumble, x, ...)
+    } else {
+      res <- lapply(BS, fitPar, x, ...)
     }
-    fitPar <- function(weights, data, ...){     
-        ind <- which(weights > 0)
-        data <- getRows(data, ind)
-        attr(data, "weight") <- weights[ind]
-        FUN(data,...)        
-    }
-    fitParJumble <- function(J, data, ...){     
-        ind <- which(J[[1]] > 0)
-        data <- getRows(data, ind)
-        attr(data, "weight") <- J[[1]][ind]
-        data <- subset(data, J[[2]])
-        FUN(data,...)        
-    }
-    if(multicore){
-        if(jumble) res <- mclapply(J, fitParJumble, x, ..., mc.cores = mc.cores) 
-        else res <- mclapply(BS, fitPar, x, ..., mc.cores = mc.cores) 
-    }        
-    else{
-        if(jumble) res <- lapply(J, fitParJumble, x, ...) 
-        else res <- lapply(BS, fitPar, x, ...)
-    } 
-    if(class(res[[1]]) == "phylo"){
-        class(res) <- "multiPhylo"   
-        res <- .compressTipLabel(res) # save memory
-    }
-    res 
+  }
+  if (class(res[[1]]) == "phylo") {
+    class(res) <- "multiPhylo"
+    res <- .compressTipLabel(res) # save memory
+  }
+  res
 }
 
 
-matchEdges <- function(tree1, tree2){
-    bp1 <- bip(tree1)
-    bp2 <- bip(tree2)
-    l <- length(tree1$tip.label)
-    fn <- function(x, y){
-        if(x[1]==1)return(x)
-        else return(y[-x])
-    } 
-    bp1[] <- lapply(bp1, fn, 1:l)
-    bp2[] <- lapply(bp2, fn, 1:l)
-    match(bp1, bp2)
+matchEdges <- function(tree1, tree2) {
+  bp1 <- bip(tree1)
+  bp2 <- bip(tree2)
+  l <- length(tree1$tip.label)
+  fn <- function(x, y) {
+    if (x[1] == 1) {
+      return(x)
+    } else {
+      return(y[-x])
+    }
+  }
+  bp1[] <- lapply(bp1, fn, 1:l)
+  bp2[] <- lapply(bp2, fn, 1:l)
+  match(bp1, bp2)
 }
 
 
-checkLabels <- function(tree, tip){
-    ind <- match(tip, tree$tip.label)
-    if (any(is.na(ind)) | length(tree$tip.label) != length(tip))
-        stop("tree has different labels")
-    tree$tip.label <- tree$tip.label[ind]
-    ind2 <- match(seq_along(ind), tree$edge[, 2])
-    tree$edge[ind2, 2] <- order(ind)
-    tree
+checkLabels <- function(tree, tip) {
+  ind <- match(tip, tree$tip.label)
+  if (any(is.na(ind)) | length(tree$tip.label) != length(tip)) {
+    stop("tree has different labels")
+  }
+  tree$tip.label <- tree$tip.label[ind]
+  ind2 <- match(seq_along(ind), tree$edge[, 2])
+  tree$edge[ind2, 2] <- order(ind)
+  tree
 }
 
 
 #' @rdname bootstrap.pml
 #' @export
-plotBS <- function (tree, BStrees, type = "unrooted", bs.col = "black", 
-                    bs.adj = NULL, p=50, frame="none",...) 
-{
-    type <- match.arg(type, c("phylogram", "cladogram", "fan", 
-                              "unrooted", "radial"))
-    if (type == "phylogram" | type == "cladogram") {
-        if (!is.rooted(tree) & !is.null(tree$edge.length)) 
-            tree2 <- midpoint(tree)
-        else tree2 <- tree
-        plot(tree2, type = type, ...)
+plotBS <- function(tree, BStrees, type = "unrooted", bs.col = "black",
+                   bs.adj = NULL, p = 50, frame = "none", ...) {
+  type <- match.arg(type, c(
+    "phylogram", "cladogram", "fan",
+    "unrooted", "radial"
+  ))
+  if (type == "phylogram" | type == "cladogram") {
+    if (!is.rooted(tree) & !is.null(tree$edge.length)) {
+      tree2 <- midpoint(tree)
+    } else {
+      tree2 <- tree
     }
-    else plot(tree, type = type, ...)
-    
-    if(hasArg(BStrees)){
-        BStrees <- .uncompressTipLabel(BStrees) # check if needed
-        if(any(is.rooted(BStrees)))BStrees <- unroot(BStrees)
-#        if(any(unlist(lapply(BStrees, is.rooted)))){
-#            BStrees <- lapply(BStrees, unroot)   
-#        }
-        x <- prop.clades(tree, BStrees)
-        x <- round((x/length(BStrees)) * 100)
-        tree$node.label <- x
-    }
-    else{
-        if(is.null(tree$node.label))stop("You need to supply 'trees' or the tree needs support-values as node.label")
-        x <- tree$node.label
-    }
-    
+    plot(tree2, type = type, ...)
+  }
+  else {
+    plot(tree, type = type, ...)
+  }
+
+  if (hasArg(BStrees)) {
+    BStrees <- .uncompressTipLabel(BStrees) # check if needed
+    if (any(is.rooted(BStrees))) BStrees <- unroot(BStrees)
+    #        if(any(unlist(lapply(BStrees, is.rooted)))){
+    #            BStrees <- lapply(BStrees, unroot)
+    #        }
+    x <- prop.clades(tree, BStrees)
+    x <- round((x / length(BStrees)) * 100)
+    tree$node.label <- x
+  }
+  else {
+    if (is.null(tree$node.label)) stop("You need to supply 'trees' or the tree needs support-values as node.label")
+    x <- tree$node.label
+  }
+
+  label <- c(rep(0, length(tree$tip.label)), x)
+  ind <- get("last_plot.phylo", envir = .PlotPhyloEnv)$edge[
+    ,
+    2
+  ]
+  if (type == "phylogram" | type == "cladogram") {
+    root <- getRoot(tree)
     label <- c(rep(0, length(tree$tip.label)), x)
-    ind <- get("last_plot.phylo", envir = .PlotPhyloEnv)$edge[, 
-                                                              2]
-    if (type == "phylogram" | type == "cladogram") {
-        root <- getRoot(tree)
-        label <- c(rep(0, length(tree$tip.label)), x)
-        label[root] <- 0
-        ind2 <- matchEdges(tree2, tree)
-        label <- label[ind2]
-        ind <- which(label > p)
-        #        browser()
-        if (is.null(bs.adj)) 
-            bs.adj <- c(1, 1)
-        if(length(ind)>0)nodelabels(text = label[ind], node = ind, 
-                            frame = frame, col = bs.col, adj = bs.adj, ...)
+    label[root] <- 0
+    ind2 <- matchEdges(tree2, tree)
+    label <- label[ind2]
+    ind <- which(label > p)
+    #        browser()
+    if (is.null(bs.adj)) {
+      bs.adj <- c(1, 1)
     }
-    else {
-        if (is.null(bs.adj)) 
-            bs.adj <- c(0.5, 0.5)
-        ind2 <- which(label[ind]>p)
-        if(length(ind2>0))edgelabels(label[ind][ind2],ind2, frame = frame, 
-                col = bs.col, adj = bs.adj, ...)
+    if (length(ind) > 0) {
+      nodelabels(
+        text = label[ind], node = ind,
+        frame = frame, col = bs.col, adj = bs.adj, ...
+      )
     }
-    invisible(tree)
+  }
+  else {
+    if (is.null(bs.adj)) {
+      bs.adj <- c(0.5, 0.5)
+    }
+    ind2 <- which(label[ind] > p)
+    if (length(ind2 > 0)) {
+      edgelabels(label[ind][ind2], ind2,
+        frame = frame,
+        col = bs.col, adj = bs.adj, ...
+      )
+    }
+  }
+  invisible(tree)
 }
 
 
 
 
 #' Maximum clade credibility tree
-#' 
+#'
 #' \code{maxCladeCred} computes the maximum clade credibility tree from a
 #' sample of trees.
-#' 
+#'
 #' So far just the best tree is returned. No annotations or transformations of
 #' edge length are performed.
-#' 
+#'
 #' If a list of partition is provided then the clade credibility is computed
 #' for the trees in x.
-#' 
+#'
 #' @param x \code{x} is an object of class \code{multiPhylo} or \code{phylo}
 #' @param tree logical indicating whether return the tree with the clade
 #' credibility (default) or the clade credibility score for all trees.
@@ -279,13 +306,13 @@ plotBS <- function (tree, BStrees, type = "unrooted", bs.col = "black",
 #' \code{\link{prop.part}}
 #' @keywords cluster
 #' @examples
-#' 
-#' 
+#'
+#'
 #' data(Laurasiatherian)
 #' set.seed(42)
-#' bs <- bootstrap.phyDat(Laurasiatherian, FUN = function(x)upgma(dist.hamming(x)),
-#'     bs=100)
-#' 
+#' bs <- bootstrap.phyDat(Laurasiatherian,
+#'   FUN = function(x)upgma(dist.hamming(x)), bs=100)
+#'
 #' strict_consensus <- consensus(bs)
 #' majority_consensus <- consensus(bs, p=.5)
 #' max_clade_cred <- maxCladeCred(bs)
@@ -293,85 +320,93 @@ plotBS <- function (tree, BStrees, type = "unrooted", bs.col = "black",
 #' plot(strict_consensus, main="Strict consensus tree")
 #' plot(majority_consensus, main="Majority consensus tree")
 #' plot(max_clade_cred, main="Maximum clade credibility tree")
-#' 
+#'
 #' # compute clade credibility for trees given a prop.part object
 #' pp <- prop.part(bs)
 #' tree <- rNNI(bs[[1]], 20)
 #' maxCladeCred(c(tree, bs[[1]]), tree=FALSE, part = pp)
 #' # first value likely be -Inf
-#' 
+#'
 #' @export maxCladeCred
-maxCladeCred <- function(x, tree=TRUE, part=NULL, rooted=TRUE){
-    if(inherits(x, "phylo")) x <- c(x)
-    if (is.null(part)){ 
-        if (!rooted) pp <- unroot(x) %>% prop.part
-        else pp <- prop.part(x)
+maxCladeCred <- function(x, tree = TRUE, part = NULL, rooted = TRUE) {
+  if (inherits(x, "phylo")) x <- c(x)
+  if (is.null(part)) {
+    if (!rooted) {
+      pp <- unroot(x) %>% prop.part()
+    } else {
+      pp <- prop.part(x)
     }
-    else pp <- part
-    pplabel <- attr(pp, "labels")
-    if(!rooted)pp <- oneWise(pp)
-    x <- .uncompressTipLabel(x)
-    class(x) <- NULL
-    m <- max(attr(pp, "number"))
-    nb <- log( attr(pp, "number") / m )
-    l <-  length(x)
-    res <- numeric(l)
-    for(i in 1:l){
-        tmp <- checkLabels(x[[i]], pplabel)
-        if(!rooted)tmp <- unroot(tmp)
-        ppi <- prop.part(tmp)  # trees[[i]]
-        if(!rooted)ppi <- oneWise(ppi)
-        indi <- fmatch(ppi, pp)
-        if(any(is.na(indi))) res[i] <- -Inf
-        else res[i] <- sum(nb[indi])
+  }
+  else {
+    pp <- part
+  }
+  pplabel <- attr(pp, "labels")
+  if (!rooted) pp <- oneWise(pp)
+  x <- .uncompressTipLabel(x)
+  class(x) <- NULL
+  m <- max(attr(pp, "number"))
+  nb <- log(attr(pp, "number") / m)
+  l <- length(x)
+  res <- numeric(l)
+  for (i in 1:l) {
+    tmp <- checkLabels(x[[i]], pplabel)
+    if (!rooted) tmp <- unroot(tmp)
+    ppi <- prop.part(tmp) # trees[[i]]
+    if (!rooted) ppi <- oneWise(ppi)
+    indi <- fmatch(ppi, pp)
+    if (any(is.na(indi))) {
+      res[i] <- -Inf
+    } else {
+      res[i] <- sum(nb[indi])
     }
-    if(tree) {
-        k <- which.max(res)
-        tr <- x[[k]]
-        attr(tr, "clade.credibility") <- res[k]
-        return(tr)
-    }    
-    res
+  }
+  if (tree) {
+    k <- which.max(res)
+    tr <- x[[k]]
+    attr(tr, "clade.credibility") <- res[k]
+    return(tr)
+  }
+  res
 }
 
 
 mcc <- maxCladeCred
 
 
-cladeMatrix <- function(x, rooted=FALSE){
-    if(!rooted) x <- unroot(x)
-    pp <- prop.part(x)
-    pplabel <- attr(pp, "labels")
-    if(!rooted)pp <- oneWise(pp)
-    x <- .uncompressTipLabel(x)
-    nnodes <- Nnode(x) 
-    class(x) <- NULL
-    #nnodes <- sapply(x, Nnode)
-    l <-  length(x)
-    from <- cumsum(c(1, nnodes[-l]))
-    to <- cumsum(nnodes)
-    
-    ivec <- integer(to[l])
-    pvec <- c(0,to)
-    
-    res <- vector("list", l)
-    k <- 1
-    for(i in 1:l){
-        ppi <- prop.part(x[[i]])  
-        if(!rooted)ppi <- oneWise(ppi)
-        indi <- sort(fmatch(ppi, pp))
-        ivec[from[i]:to[i]] <- indi
-    }
-    X <- sparseMatrix(i=ivec, p=pvec, dims=c(length(pp),l))
-    list(X=X, prop.part=pp)
+cladeMatrix <- function(x, rooted = FALSE) {
+  if (!rooted) x <- unroot(x)
+  pp <- prop.part(x)
+  pplabel <- attr(pp, "labels")
+  if (!rooted) pp <- oneWise(pp)
+  x <- .uncompressTipLabel(x)
+  nnodes <- Nnode(x)
+  class(x) <- NULL
+  # nnodes <- sapply(x, Nnode)
+  l <- length(x)
+  from <- cumsum(c(1, nnodes[-l]))
+  to <- cumsum(nnodes)
+
+  ivec <- integer(to[l])
+  pvec <- c(0, to)
+
+  res <- vector("list", l)
+  k <- 1
+  for (i in 1:l) {
+    ppi <- prop.part(x[[i]])
+    if (!rooted) ppi <- oneWise(ppi)
+    indi <- sort(fmatch(ppi, pp))
+    ivec[from[i]:to[i]] <- indi
+  }
+  X <- sparseMatrix(i = ivec, p = pvec, dims = c(length(pp), l))
+  list(X = X, prop.part = pp)
 }
 
 
-moving_average <- function(obj, window=50){
-    fun <- function(x){
-        cx <- c(0, cumsum(x))
-        (cx[(window+1):length(cx)] - cx[1:(length(cx)-window)])/(window)
-    }
-    res <- apply(obj$X, 1, fun)
-    rownames(res) <- c()
+moving_average <- function(obj, window = 50) {
+  fun <- function(x) {
+    cx <- c(0, cumsum(x))
+    (cx[(window + 1):length(cx)] - cx[1:(length(cx) - window)]) / (window)
+  }
+  res <- apply(obj$X, 1, fun)
+  rownames(res) <- c()
 }
--- R/cladePar.R
+++ R/cladePar.R
@@ -1,8 +1,8 @@
 #' Utility function to plot.phylo
-#' 
+#'
 #' cladePar can help you coloring (choosing edge width/type) of clades.
-#' 
-#' 
+#'
+#'
 #' @param tree an object of class phylo.
 #' @param node the node which is the common ancestor of the clade.
 #' @param edge.color see plot.phylo.
@@ -17,35 +17,40 @@
 #' @seealso \code{\link{plot.phylo}}
 #' @keywords plot
 #' @examples
-#' 
+#'
 #' tree <- rtree(10)
 #' plot(tree)
 #' nodelabels()
 #' x <- cladePar(tree, 12)
 #' cladePar(tree, 18, "blue", "blue", x=x, plot=TRUE)
-#' 
+#'
 #' @export cladePar
-cladePar <- function(tree, node, edge.color="red", tip.color=edge.color, 
-        edge.width = 1, edge.lty = 1, x=NULL, plot=FALSE, ...){
-    if(is.null(x)){
-        m <- max(tree$edge)
-        x <- list(edge=data.frame(color=rep("black",m), width = rep(1, m), 
-            lty = rep(1, m), stringsAsFactors = FALSE), 
-            tip=rep("black", length(tree$tip.label)))         
-    }
-    ind <- Descendants(tree,node,"all")
-    x$edge$color[ind] <- edge.color
-    x$edge$width[ind] <- edge.width
-    x$edge$lty[ind] <- edge.lty
-    x[[2]][Descendants(tree, node, "tips")[[1]]] <- tip.color
-    if(plot){
-        tree <- reorder(tree)
-        plot(tree, edge.color=x$edge$color[tree$edge[,2]], 
-             edge.width = x$edge$width[tree$edge[,2]], 
-             edge.lty = x$edge$lty[tree$edge[,2]], tip.color=x[[2]],...)
-    }
-    else return(x)
-} 
-
-
-
+cladePar <- function(tree, node, edge.color = "red", tip.color = edge.color,
+                    edge.width = 1, edge.lty = 1, x = NULL, plot = FALSE, ...) {
+  if (is.null(x)) {
+    m <- max(tree$edge)
+    x <- list(
+      edge = data.frame(
+        color = rep("black", m), width = rep(1, m),
+        lty = rep(1, m), stringsAsFactors = FALSE
+      ),
+      tip = rep("black", length(tree$tip.label))
+    )
+  }
+  ind <- Descendants(tree, node, "all")
+  x$edge$color[ind] <- edge.color
+  x$edge$width[ind] <- edge.width
+  x$edge$lty[ind] <- edge.lty
+  x[[2]][Descendants(tree, node, "tips")[[1]]] <- tip.color
+  if (plot) {
+    tree <- reorder(tree)
+    plot(tree,
+      edge.color = x$edge$color[tree$edge[, 2]],
+      edge.width = x$edge$width[tree$edge[, 2]],
+      edge.lty = x$edge$lty[tree$edge[, 2]], tip.color = x[[2]], ...
+    )
+  }
+  else {
+    return(x)
+  }
+}
