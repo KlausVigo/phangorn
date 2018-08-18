f976e400ea5c3ccd76591743ae54a6955135200e
KlausVigo <klaus.schliep@gmail.com>
2018-08-18 16:42:34 -0400

    improve tests

--- tests/testthat/test_codon.R
+++ tests/testthat/test_codon.R
@@ -3,24 +3,25 @@ context("codon")
 
 tree <- read.tree(text = "(((t1:.1,t2:.1):.1,t3:.2):.1,(t4:.1,t5:.1):.2);")
 set.seed(42)
-dat_1 <- simSeq(tree, l=1000, type = "CODON")
-dat_2 <- simSeq(tree, l=1000, type = "CODON", dnds=.5)
-dat_3 <- simSeq(tree, l=1000, type = "CODON", tstv=2)
-dat_4 <- simSeq(tree, l=1000, type = "CODON", dnds=2, tstv=2)
 
+dat_1 <- simSeq(tree, l=1000, type = "CODON", dnds=0.5, tstv=2)
+dat_2 <- simSeq(tree, l=1000, type = "CODON", dnds=1, tstv=2)
+dat_3 <- simSeq(tree, l=500, type = "CODON", dnds=3, tstv=2)
 
+dat_4 <- c(dat_1, dat_2, dat_3)
 
-fit_F1x4 <- pml(tree, dat_4, bf="F1x4")
-fit_F3x4 <- pml(tree, dat_4, bf="F3x4")
-fit_GY <- pml(tree, dat_4, bf="empirical")
-
+fit_F1x4 <- pml(tree, dat_1, bf="F1x4")
+fit_F3x4 <- pml(tree, dat_1, bf="F3x4")
+fit_GY <- pml(tree, dat_1, bf="empirical")
 
 test_that("edge length optimisation works properly", {
     skip_on_cran()
     
     fit_GY_opt <- optim.pml(fit_GY, model="codon1")
-    expect_gt(fit_GY_opt$dnds, 1)
+    expect_lt(fit_GY_opt$dnds, 1)
     expect_gt(fit_GY_opt$tstv, 1)
+    
+    fit_selection <- codonTest(tree, dat_4)
 })
 
 
