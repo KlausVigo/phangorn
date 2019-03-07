d7392dfceb47721243d2d489a108fbc58c2fb283
klash <klaus.schliep@gmail.com>
2019-03-07 18:11:12 -0500

    fix NAMSESPACE

--- R/utils.R
+++ R/utils.R
@@ -12,7 +12,16 @@ NULL
 #' @import Rcpp
 #' @import parallel
 #' @import ape
-## @importFrom ape rtree
+#' @importFrom stats AIC BIC logLik reorder update optim optimize constrOptim
+#' @importFrom stats cophenetic hclust as.dist pchisq reshape qgamma pgamma
+#' @importFrom stats na.omit model.matrix aggregate lm.fit xtabs quantile sd
+#' @importFrom stats runif qbeta
+#' @importFrom graphics plot plot.default plot.new plot.window text par abline
+#' @importFrom graphics strwidth axis title segments points image matplot legend
+#' @importFrom graphics hist identify locator barplot
+#' @importFrom utils read.table download.file stack
+#' @importFrom utils installed.packages write.table combn packageDescription
+#' @importFrom grDevices rgb rainbow adjustcolor col2rgb
 
 #' @export
 ape::rtree
