1dbe45c3ba86d44ad5dffebd07dd9198b7e73431
Klaus Schliep <klaus.schliep@gmail.com>
2020-04-03 17:24:07 +0200

    fix error in bootstrap if states > 31

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -1,10 +1,17 @@
 candidate.tree <- function(x){
-  tree <- random.addition(x)
-  tree <- optim.parsimony(tree, x)
-  tree <- multi2di(tree)
-  tree <- unroot(tree)
-  tree <- acctran(tree, x)
-  tree$edge.length <- tree$edge.length / sum(attr(x, "weight"))
+  if(attr(x, "nc") > 31){
+     dm <- dist.ml(x)
+     tree <- fastme.bal(dm, nni = TRUE, spr = FALSE, tbr = FALSE)
+     tree$edge.length[tree$edge.length<0] <- 1e-8
+  }
+  else{
+    tree <- random.addition(x)
+    tree <- optim.parsimony(tree, x)
+    tree <- multi2di(tree)
+    tree <- unroot(tree)
+    tree <- acctran(tree, x)
+    tree$edge.length <- tree$edge.length / sum(attr(x, "weight"))
+  }
   tree
 }
 
