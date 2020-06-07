836a0d8d66a1cb62c16be18e20793f2df00db9d5
Klaus Schliep <klaus.schliep@gmail.com>
2020-06-07 14:18:25 +0200

    speed up parsinfo, compute lower bound only where necessary

--- R/parsimony.R
+++ R/parsimony.R
@@ -147,13 +147,22 @@ compressSites <- function(data) {
 }
 
 
-#
-# Branch and bound
-#
+# parsinfo <- function(x) {
+#  low <- lowerBound(x)
+#  up <- upperBound(x)
+#  ind <- which(low == up)
+#  cbind(ind, low[ind])
+#}
+
 
 parsinfo <- function(x) {
-  low <- lowerBound(x)
+  nstates <- attr(x, "nc")
   up <- upperBound(x)
+  eps <- 1e-8
+  low <- up
+  low[up > (nstates - eps)] <- nstates - 1
+  ind <- which( (up > (1+eps))  & (up < (nstates-eps)) )
+  low[ind] <- lowerBound(getRows( x, ind ))
   ind <- which(low == up)
   cbind(ind, low[ind])
 }
