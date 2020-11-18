5c4ccde13094072c6e82e617d001c7a4f01316c4
Klaus Schliep <klaus.schliep@gmail.com>
2020-11-18 19:12:08 +0100

    clean up

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,3 @@
 #' @importFrom magrittr %>%
 #' @export
 magrittr::'%>%'
-
-
-
-
--- R/zzz.R
+++ R/zzz.R
@@ -19,8 +19,8 @@
 .packageName <- "phangorn"
 
 .aamodels <- c("WAG", "JTT", "LG", "Dayhoff", "cpREV", "mtmam", "mtArt",
-               "MtZoa", "mtREV24", "VT","RtREV", "HIVw", "HIVb", "FLU",
-               "Blosum62","Dayhoff_DCMut","JTT_DCMut")
+               "MtZoa", "mtREV24", "VT", "RtREV", "HIVw", "HIVb", "FLU",
+               "Blosum62", "Dayhoff_DCMut", "JTT_DCMut")
 
 .dnamodels <- c("JC", "F81", "K80", "HKY", "TrNe", "TrN",
   "TPM1", "K81", "TPM1u", "TPM2", "TPM2u", "TPM3", "TPM3u",
