17c9e8831eb86937cb6435d5a293dd610906a362
Klaus Schliep <klaus.schliep@gmail.com>
2021-05-18 17:43:40 +0200

    fix issue #117

--- R/ancestral_pml.R
+++ R/ancestral_pml.R
@@ -367,7 +367,7 @@ acctran2 <- function(tree, data) {
   f <- init_fitch(data, FALSE, FALSE, m=2L)
   psc_node <- f$pscore_node(edge)
   tmp <- reorder(tree)$edge
-  tmp <- tmp[tmp[,2]>Ntip(tree),]
+  tmp <- tmp[tmp[,2]>Ntip(tree), ,drop=FALSE]
   f$traverse(edge)
   if(length(tmp)>0)f$acctran_traverse(tmp)
   psc <- f$pscore_acctran(edge)
