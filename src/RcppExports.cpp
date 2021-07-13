81a8081d05fa43c719af76d315b8fd691ed3f96d
Klaus Schliep <klaus.schliep@gmail.com>
2021-07-13 13:26:03 +0200

    prepare for CRAN submission

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // fhm_new
 NumericVector fhm_new(NumericVector v, int n);
 RcppExport SEXP _phangorn_fhm_new(SEXP vSEXP, SEXP nSEXP) {
