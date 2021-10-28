b9154f96dfec5781a6ab0c11c99321bb3170bf2f
Klaus Schliep <klaus.schliep@gmail.com>
2021-10-28 11:15:50 +0200

    automatize landing page

--- R/phangorn-package.R
+++ R/phangorn-package.R
@@ -1,5 +1,3 @@
-
-
 #' Chloroplast alignment
 #'
 #' Amino acid alignment of 15 genes of 19 different chloroplast.
@@ -14,8 +12,6 @@
 #' chloroplast
 #'
 NULL
-
-
 
 
 
@@ -37,41 +33,8 @@ NULL
 NULL
 
 
-
-
-
-#' Phylogenetic analysis in R
-#'
-#' Phylogenetic analysis in R (Estimation of phylogenetic trees and networks
-#' using Maximum Likelihood, Maximum Parsimony, Distance methods & Hadamard
-#' conjugation)
-#'
-#' The complete list of functions can be displayed with \code{library(help =
-#' phangorn)}.
-#'
-#' Further information is available in several vignettes. To show the available
-#' vignettes in an HTML browser type \code{browseVignettes("phangorn")}.
-#' \tabular{ll}{ \code{Trees} \tab Constructing phylogenetic trees (source,
-#' pdf) \cr \code{phangorn-specials} \tab Advanced features (source, pdf) \cr
-#' \code{Ancestral} \tab Ancestral sequence reconstruction (source, pdf) \cr
-#' \code{Networx} \tab Splits and Networx (source, html) \cr } The first
-#' vignette (to display type \code{vignette('Trees')}) gives an introduction in
-#' phylogenetic analysis with phangorn. The second vignette (phangorn-special)
-#' covers more advanced feature like defining special character spaces and
-#' things which fit nowhere else.  More information on \bold{phangorn} can be
-#' found on \url{https://www.phangorn.org/}.
-#'
-#'
-#' @name phangorn-package
-#' @aliases phangorn-package phangorn
-#' @docType package
-#' @author Klaus Schliep
-#'
-#' Maintainer: Klaus Schliep <klaus.schliep@@gmail.com>
-#' @references Schliep K.P. (2011) phangorn: Phylogenetic analysis in R.
-#' \emph{Bioinformatics}, 27(4) 592-593
-#' @keywords package
-NULL
+#' @keywords internal
+"_PACKAGE"
 
 
 #' Internal phangorn Functions
