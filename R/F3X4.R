64a67dd831033a1160e00239e1c294765dbcca0f
Klaus Schliep <klaus.schliep@gmail.com>
2021-11-04 15:02:27 +0100

    clean up

--- R/F3X4.R
+++ R/F3X4.R
@@ -1,35 +1,3 @@
-
-# estimate codon frequencies based on the F3X4 method
-getF3X4codon_frequencies <- function(codon_data,
-                              codon_alphabet = attr(codon_data, "levels"),
-                              nucleotide_alphabet = .nucleotideAlphabet){
-  epirical_codon_frequenices <- baseFreq(codon_data)
-  # get the position-wise nucleotide frequencies
-  nuc_freq_by_pos <- matrix(0, nrow = length(nucleotide_alphabet), ncol = 3)
-  for (i in seq_along(codon_alphabet)) {
-    codon <- codon_alphabet[i]
-    codon_freq <- epirical_codon_frequenices[i]
-    for (j in 1:3) {
-      nuc <- substr(codon, j, j)
-      nuc_index <- which(nucleotide_alphabet == nuc)
-      nuc_freq_by_pos[nuc_index, j] <- nuc_freq_by_pos[nuc_index, j] +
-        codon_freq
-    }
-  }
-  # construct the codon frequencies according to the F3X4 method
-  codon_frequencies <- rep(0, length(codon_alphabet))
-  for (i in seq_along(codon_alphabet)) {
-    codon <- codon_alphabet[i]
-    codon_components <- unlist(strsplit(codon, ""))
-    nuc1_freq <- nuc_freq_by_pos[which(nucleotide_alphabet == codon_components[1]), 1]
-    nuc2_freq <- nuc_freq_by_pos[which(nucleotide_alphabet == codon_components[2]), 2]
-    nuc3_freq <- nuc_freq_by_pos[which(nucleotide_alphabet == codon_components[3]), 3]
-    codon_frequencies[i] <- nuc1_freq * nuc2_freq * nuc3_freq
-  }
-  codon_frequencies <-  codon_frequencies / sum(codon_frequencies)
-  return(codon_frequencies)
-}
-
 # compute frequencies for codon positions
 # TODO: using baseFreq codon should save memory instead of codon2dna
 bf_by_codon <- function(x) {
@@ -62,26 +30,9 @@ F3x4 <- function(x) {
 }
 
 
-#F1x4_freq <- function(M, CodonAlphabet = .CodonAlphabet,
-#                      nucleotideAlphabet = .nucleotideAlphabet) {
-#  pos <- CodonAlphabet |> strsplit("") |> unlist() |>
-#    match(nucleotideAlphabet) |> matrix(ncol = 3, byrow = TRUE)
-#  codon_frequencies <- M[pos[, 1]] * M[pos[, 2]] * M[pos[, 3]]
-#  codon_frequencies / sum(codon_frequencies)
-#}
-
-
 F1x4 <- function(x) {
   bf <- codon2dna(x) |> baseFreq()
   BF <- matrix(bf, 4, 3)
   codon_abc <- attr(x, "levels")
   F3x4_freq(BF, CodonAlphabet = codon_abc)
 }
-
-
-
-# test
-# SCRIPT_DIR = # please fill in the script dir
-# codon_data_path = SCRIPT_DIR + "/codon_aln.fas"
-# codon_data = read.phyDat(codon_data_path, format = "fasta", type="CODON")
-# codon_frequencies = getF3X4codon_frequencies(codon_data)
