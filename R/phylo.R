0e74eb08abf29811fa1d8518228c4ee04ba17ea6
Klaus Schliep <klaus.schliep@gmail.com>
2022-07-11 12:32:31 +0200

    reorganize code

--- R/phylo.R
+++ R/phylo.R
@@ -705,40 +705,6 @@ pml.move <- function(EDGE, el, data, g, w, eig, k, nTips, bf) {
 }
 
 
-bip <- function(x) {
-  x <- reorder(x, "postorder")
-  nTips <- as.integer(length(x$tip.label))
-  res <- .Call("_phangorn_bipCPP", x$edge, nTips)
-  attr(res, "labels") <- x$tip.label
-  res
-}
-
-# replace with prop.part
-bipart <- function(x) {
-  x <- reorder(x, "postorder")
-  nTips <- as.integer(length(x$tip.label))
-  res <- .Call('_phangorn_bipartCPP', x$edge, nTips)
-  attr(res, "labels") <- x$tip.label
-  res
-}
-
-
-bipartition <- function(tree) {
-  if (is.rooted(tree)) tree <- unroot(tree)
-  tree <- reorder(tree, "postorder")
-  bp <- bip(tree)
-  nTips <- length(tree$tip.label)
-  l <- length(bp)
-  res <- matrix(0L, l, nTips)
-  for (i in 1:l) res[i, bp[[i]]] <- 1L
-  res <- res[tree$edge[, 2], , drop = FALSE]
-  colnames(res) <- tree$tip.label
-  rownames(res) <- tree$edge[, 2]
-  res[res[, 1] == 1, ] <- 1L - res[res[, 1] == 1, ]
-  res
-}
-
-
 readAArate <- function(file) {
   tmp <- read.table(system.file(file.path("extdata", file)), col.names = 1:20,
     fill = TRUE)
