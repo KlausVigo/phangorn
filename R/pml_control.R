a661b4a54d82e297dbb66b039a8df9357226b45b
Klaus Schliep <klaus.schliep@gmail.com>
2022-07-12 18:55:59 +0200

    reorganize code

---
+++ R/pml_control.R
@@ -0,0 +1,58 @@
+#' Auxiliary for Controlling Fitting
+#'
+#' Auxiliary functions for \code{\link{optim.pml}} fitting. Use it to construct
+#' a \code{control} or \code{ratchet.par} argument.
+#'
+#' \code{pml.control} controls the fitting process. \code{epsilon} and
+#' \code{maxit} are only defined for the most outer loop, this affects
+#' \code{pmlCluster}, \code{pmlPart} and \code{pmlMix}.  \code{epsilon} is
+#' defined as (logLik(k)-logLik(k+1))/logLik(k+1), this seems to be a good
+#' heuristics which works reasonably for small and large trees or alignments.
+#' If \code{trace} is set to zero than no out put is shown, if functions are
+#' called internally than the trace is decreased by one, so a higher of trace
+#' produces more feedback.
+#'
+### @param control A list of parameters for controlling the fitting process.
+#' @param epsilon Stop criterion for optimization (see details).
+#' @param maxit Maximum number of iterations (see details).
+#' @param trace Show output during optimization (see details).
+#' @param tau minimal edge length.
+#' @param minit Minimum number of iterations.
+#' @param iter Number of iterations to stop if there is no change.
+#' @param prop Only used if \code{rearrangement=stochstic}. How many NNI moves
+#' should be added to the tree in proportion of the number of taxa.´
+#' @param rell logical, if TRUE approximate boostrapping similar Minh et al.
+#' (2013) is performed.
+#' @return A list with components named as the arguments for controlling the
+#' fitting process.
+#' @author Klaus Schliep \email{klaus.schliep@@gmail.com}
+#' @seealso \code{\link{optim.pml}}
+#' @references Minh, B. Q., Nguyen, M. A. T., & von Haeseler, A. (2013). Ultrafast approximation for phylogenetic bootstrap. \emph{Molecular biology and evolution}, \bold{30(5)}, 1188-1195.
+#' @examples
+#' pml.control()
+#' pml.control(maxit=25)
+#' @export
+pml.control <- function(epsilon = 1e-08, maxit = 10, trace = 1, tau = 1e-8) {
+  if (!is.numeric(epsilon) || epsilon <= 0)
+    stop("value of 'epsilon' must be > 0")
+  if (!is.numeric(maxit) || maxit <= 0)
+    stop("maximum number of iterations must be > 0")
+  if (!is.numeric(tau) || tau <= 0)
+    stop("tau must be > 0")
+  list(epsilon = epsilon, maxit = maxit, trace = trace, tau = tau)
+}
+
+#' @rdname pml.control
+#' @export
+ratchet.control <- function(iter = 20L, maxit = 200L, minit = 100L, prop = 1/2,
+                            rell = TRUE){
+  if (!is.numeric(maxit) || maxit <= 0)
+    stop("maximum number of iterations must be > 0")
+  if (!is.numeric(minit) || minit <= 0)
+    stop("minimum number of iterations must be > 0")
+  if (!is.numeric(iter) || iter <= 0)
+    stop("number of iterations must be > 0")
+  if (!is.numeric(iter) || iter <= 0)
+    stop("proportion of rearrangenemts must be > 0")
+  list(iter = iter, maxit = maxit, minit = minit, prop = prop, rell = rell)
+}
---
+++ R/pml_generics.R
@@ -0,0 +1,117 @@
+#' @export
+logLik.pml <- function(object, ...) {
+  res <- object$logLik
+  attr(res, "df") <- object$df
+  class(res) <- "logLik"
+  res
+}
+
+
+#' @export
+AICc <- function(object, ...)
+  UseMethod("AICc")
+
+
+#' @export
+AICc.pml <- function(object, ...) {
+  n <- sum(object$weight)
+  k <- object$df
+  if (k >= (n - 1)) return(NaN)
+  res <- AIC(object)
+  res +   (2 * k * (k + 1)) / (n - k - 1)
+}
+
+
+#' @export
+BIC.pml <- function(object, ...) {
+  res <- AIC(object, k = log(sum(object$weight)))
+  res
+}
+
+
+#' @export
+anova.pml <- function(object, ...) {
+  X <- c(list(object), list(...))
+  fun <- function(x) {
+    tmp <- logLik(x)
+    c(tmp[1], attr(tmp, "df"))
+  }
+  DF <- t(sapply(X, fun))
+  dev <- c(NA, 2 * diff(DF[, 1]))
+  ddf <- c(NA, diff(DF[, 2]))
+  table <- data.frame(DF, ddf, dev, pchisq(dev, ddf, lower.tail = FALSE))
+  dimnames(table) <- list(seq_along(X), c("Log lik.", "Df",
+                                          "Df change", "Diff log lik.", "Pr(>|Chi|)"))
+  structure(table, heading = "Likelihood Ratio Test Table",
+            class = c("anova", "data.frame"))
+}
+
+
+#' @export
+vcov.pml <- function(object, ...) {
+  FI <- score(object, FALSE)[[2]]
+  l <- dim(FI)[1]
+  res <- try(solve(FI))
+  if (inherits(res, "try-error")) {
+    cat("Covariance is ill-conditioned !! \n")
+    res <- solve(FI + diag(l) * 1e-8)
+  }
+  res
+}
+
+
+#' @export
+plot.pml <- function(x, ...){
+  plot.phylo(x$tree, ...)
+  if(is.rooted(x$tree)) axisPhylo()
+  else add.scale.bar()
+}
+
+
+#' @export
+print.pml <- function(x, ...) {
+  model <- guess_model(x)
+  cat("model:", model, "\n")
+  cat("loglikelihood:", x$logLik, "\n")
+  w <- x$weight
+  w <- w[w > 0]
+  type <- attr(x$data, "type")
+  levels <- attr(x$data, "levels")
+  nc <- attr(x$data, "nc")
+  ll0 <- sum(w * log(w / sum(w)))
+  cat("unconstrained loglikelihood:", ll0, "\n")
+  if (x$inv > 0) cat("Proportion of invariant sites:", x$inv, "\n")
+  if (x$k > 1) {
+    cat("Discrete gamma model\n")
+    cat("Number of rate categories:", x$k, "\n")
+    cat("Shape parameter:", x$shape, "\n")
+  }
+  if (type == "AA") cat("Rate matrix:", x$model, "\n")
+  if (type == "DNA") {
+    cat("\nRate matrix:\n")
+    QM <- matrix(0, nc, nc, dimnames = list(levels, levels))
+    QM[lower.tri(QM)] <- x$Q
+    QM <- QM + t(QM)
+    print(QM)
+    cat("\nBase frequencies:  \n")
+    bf <- x$bf
+    names(bf) <- levels
+    print(bf) #cat(bf, "\n")
+  }
+  if (type == "CODON") {
+    cat("dn/ds:", x$dnds, "\n")
+    cat("ts/tv:", x$tstv, "\n")
+    cat("Freq:", x$frequencies, "\n")
+  }
+  if (type == "USER" & length(x$bf) < 11) {
+    cat("\nRate matrix:\n")
+    QM <- matrix(0, nc, nc, dimnames = list(levels, levels))
+    QM[lower.tri(QM)] <- x$Q
+    QM <- QM + t(QM)
+    print(QM)
+    cat("\nBase frequencies:  \n")
+    bf <- x$bf
+    names(bf) <- levels
+    print(bf) #cat(bf, "\n")
+  }
+}
