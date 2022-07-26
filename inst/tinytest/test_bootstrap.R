4c3c1855f855e90d70f7527b6cb6514d8160e1d6
Klaus Schliep <klaus.schliep@gmail.com>
2022-07-26 13:41:03 +0200

    add test

--- inst/tinytest/test_bootstrap.R
+++ inst/tinytest/test_bootstrap.R
@@ -6,8 +6,15 @@ bs_trees <- bootstrap.phyDat(Laurasiatherian, fun)
 tree1 <- plotBS(tree, bs_trees, type="none")
 tree2 <- plotBS(tree, bs_trees, type="none", method="TBE")
 
+nnet <- neighborNet(dist.hamming(Laurasiatherian))
 
 expect_true(inherits(tree1, "phylo"))
 # transfer bootstrap should never be smaller than the standard one
 expect_true(all(tree1$node.label[-1] <= tree2$node.label[-1]))
 
+
+tree3 <- addConfidences(tree, bs_trees)
+expect_equal(tree1, tree3)
+nnet2 <- addConfidences(nnet, bs_trees)
+expect_true(is.null(attr(nnet$splits, "confidences")))
+expect_false(is.null(attr(nnet2$splits, "confidences")))
