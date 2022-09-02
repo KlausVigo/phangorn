e768207bb3bd007eb99d7e737c009b73d83b5494
Klaus Schliep <klaus.schliep@gmail.com>
2022-09-02 14:44:06 +0200

    add better support for Mkv and type="USER" models

--- R/zzz.R
+++ R/zzz.R
@@ -22,10 +22,13 @@
                "MtZoa", "mtREV24", "VT", "RtREV", "HIVw", "HIVb", "FLU",
                "Blosum62", "Dayhoff_DCMut", "JTT_DCMut")
 
-.dnamodels <- c("JC", "F81", "K80", "HKY", "TrNe", "TrN",
+.dnamodels <- c("JC", "Mk", "Mkv", "F81", "K80", "HKY", "TrNe", "TrN",
   "TPM1", "K81", "TPM1u", "TPM2", "TPM2u", "TPM3", "TPM3u",
   "TIM1e", "TIM1", "TIM2e", "TIM2", "TIM3e", "TIM3", "TVMe",
   "TVM", "SYM", "GTR")
+
+.usermodels <- c("ER", "Mk", "Mkv", "SYM", "GTR", "FREQ") # meristic??
+
 
 # environment variables
 # .CodonAlphabet <- c("aaa", "aac", "aag", "aat", "aca", "acc", "acg", "act",
