b20bbf196b75e08cb1427aecb6794a8c4b50a7b6
Klaus Schliep <klaus.schliep@gmail.com>
2022-09-08 14:46:16 +0200

    add test for user defined data

--- inst/tinytest/test_modelTest.R
+++ inst/tinytest/test_modelTest.R
@@ -10,28 +10,38 @@ set.seed(42)
 Y <- simSeq(tree, l=500, type = "AA", model="WAG")
 
 # test modelTest
-    MT <- modelTest(X, tree = tree, I=FALSE,
+MT <- modelTest(X, tree = tree, I=FALSE,
                 control = pml.control(epsilon = 1e-08, maxit = 10, trace = 0),
                 multicore = TRUE, mc.cores = 2L)
-    expect_equal(MT$Model[which.min(MT$BIC)], "F81+G(4)")
+expect_equal(MT$Model[which.min(MT$BIC)], "F81+G(4)")
 
-    fitMT <- as.pml(MT)
-    expect_true(inherits(fitMT, "pml"))
-    expect_equal(fitMT$model, "F81")
+fitMT <- as.pml(MT)
+expect_true(inherits(fitMT, "pml"))
+expect_equal(fitMT$model, "F81")
 
-    MT2 <- modelTest(X, tree = tree, model = c("JC", "F81", "K80", "HKY", "SYM",
+MT2 <- modelTest(X, tree = tree, model = c("JC", "F81", "K80", "HKY", "SYM",
           "GTR"), control = pml.control(epsilon = 1e-08, maxit = 10, trace = 0),
           multicore = TRUE, mc.cores = 2L)
-    expect_equal(MT2$Model[which.min(MT2$BIC)], "F81+G(4)")
+expect_equal(MT2$Model[which.min(MT2$BIC)], "F81+G(4)")
 
-    # amino acid models with FREQ
-    MT_AA <- modelTest(Y, model=c("JTT", "WAG"), FREQ = TRUE,
+# amino acid models with FREQ
+MT_AA <- modelTest(Y, model=c("JTT", "WAG"), FREQ = TRUE,
             control = pml.control(epsilon = 1e-08, maxit = 5, trace = 0),
             multicore = TRUE, mc.cores = 2L)
-    expect_equal(MT_AA$Model[which.min(MT_AA$BIC)], "WAG")
-    # test all
-    MT_AA_all <- modelTest(Y, I=FALSE, G=FALSE,
-            control = pml.control(epsilon = 1e-08, maxit = 5, trace = 0),
-            multicore = TRUE, mc.cores = 2L)
-    expect_equal(MT_AA_all$Model[which.min(MT_AA_all$BIC)], "WAG")
+expect_equal(MT_AA$Model[which.min(MT_AA$BIC)], "WAG")
+# test all
+MT_AA_all <- modelTest(Y, I=FALSE, G=FALSE,
+             control = pml.control(epsilon = 1e-08, maxit = 5, trace = 0),
+             multicore = TRUE, mc.cores = 2L)
+expect_equal(MT_AA_all$Model[which.min(MT_AA_all$BIC)], "WAG")
+
+
+# test user defined states
+tree <- rcoal(10)
+Z <- simSeq(tree, Q = c(1,0,0,1,0,1), type = "USER",
+            levels=c("A", "B", "C", "D"))
+MT_USER <- modelTest(Z, I=TRUE, G=TRUE,
+                  control = pml.control(epsilon = 1e-08, maxit = 5, trace = 0),
+                  multicore = TRUE, mc.cores = 2L)
+expect_equal(MT_USER$Model[which.min(MT_USER$BIC)], "ORDERED")
 
