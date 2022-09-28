9d79ab0f1957f8a8b9c19f76af281b4eb7922141
Klaus Schliep <klaus.schliep@gmail.com>
2022-09-28 18:09:02 +0200

    start adding tests

--- inst/tinytest/test_ancestral.R
+++ inst/tinytest/test_ancestral.R
@@ -1,4 +1,7 @@
 tree <- read.tree(text = "((t1:1,t2:1):1,(t3:1,t4:1):1);")
+tree2 <- read.tree(text = "(((t1:1,t2:1):1,t3:2):1,t4:3);")
+
+
 dat <- matrix(c("a", "a",
                 "a", "t",
                 "t", "a",
@@ -11,11 +14,16 @@ fit <- pml(tree, dna)
 
 # dna tests differs from other data types as it may returns ambiguous data
 # test ancestral generics
-    test.ml1 <- ancestral.pml(fit, type = "ml")
-    test.ml2 <- ancestral.pml(fit, type = "ml", return = "phyDat")
-#    test1 <- ancestral.pars(tree, dna, "MPR", return = "prob")
-#    test2 <- ancestral.pars(tree, dna, "MPR", return = "phyDat")
-#    test3 <- ancestral.pars(tree, dna, "ACCTRAN", return = "prob")
-#    test4 <- ancestral.pars(tree, dna, "ACCTRAN", return = "phyDat")
-#    expect_equal(as.character(test2), as.character(test4))
-#    expect_equal(as.character(test.ml2), as.character(test2))
+#    test.ml1 <- ancestral.pml(fit, type = "ml")
+test_ml <- ancestral.pml(fit, type = "ml", return = "phyDat")
+test_mpr <- ancestral.pars(tree, dna, "MPR", return = "phyDat")
+test_acctran <- ancestral.pars(tree, dna, "ACCTRAN", return = "phyDat")
+
+expect_equal(as.character(test_ml), as.character(test_acctran))
+expect_equal(as.character(test_ml), as.character(test_mpr))
+
+test_mpr_2 <- ancestral.pars(tree2, dna, "MPR", return = "phyDat")
+test_acctran_2 <- ancestral.pars(tree2, dna, "ACCTRAN", return = "phyDat")
+
+expect_equal(test_mpr_2[,1], test_acctran_2[,1], check.attributes = FALSE)
+
