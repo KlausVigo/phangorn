b9cdc8bc8fb7d066cc05955b3931f272c3b462cb
Klaus Schliep <klaus.schliep@gmail.com>
2023-01-24 14:12:48 +0100

    clean up, add test

--- R/modelTest.R
+++ R/modelTest.R
@@ -4,8 +4,6 @@ aic.weights <- function(aic) {
 }
 
 
-
-
 #' ModelTest
 #'
 #' Comparison of different nucleotide or amino acid substitution models
@@ -124,36 +122,23 @@ modelTest <- function(object, tree = NULL, model = NULL, G = TRUE, I = TRUE,
     data.frame(c("Model", "df", "logLik", "AIC", "AICc", "BIC"))
     calls <- vector("list", n)
     trees <- vector("list", n)
-#    if (trace > 0) print(model)
     fittmp <- optim.pml(fit, model = model, control = control)
     res[m, 1] <- model
     pars <- get_pars(fittmp, nseq)
     res[m, 2:6] <- pars
     if (trace > 0) cat(formatC(res[m,1], width=12),
                        prettyNum(pars[-4], preserve.width="individual"), "\n")
-#    res[m, 2] <- fittmp$df
-#    res[m, 3] <- fittmp$logLik
-#    res[m, 4] <- AIC(fittmp)
-#    res[m, 5] <- AICc(fittmp)
-#    res[m, 6] <- AIC(fittmp, k = log(nseq))
     calls[[m]] <- fittmp$call
-
     trees[[m]] <- fittmp$tree
     m <- m + 1
     if (I) {
-#      if (trace > 0) print(paste0(model, "+I"))
       fitI <- optim.pml(fittmp, model = model, optInv = TRUE,
         control = control)
       res[m, 1] <- paste0(model, "+I")
       pars <- get_pars(fitI, nseq)
       res[m, 2:6] <- pars
-      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4], preserve.width="individual"),
-                         "\n")
-#      res[m, 2] <- fitI$df
-#      res[m, 3] <- fitI$logLik
-#      res[m, 4] <- AIC(fitI)
-#      res[m, 5] <- AICc(fitI)
-#      res[m, 6] <- AIC(fitI, k = log(nseq))
+      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4],
+                         preserve.width="individual"), "\n")
       calls[[m]] <- fitI$call
       trees[[m]] <- fitI$tree
       m <- m + 1
@@ -166,88 +151,59 @@ modelTest <- function(object, tree = NULL, model = NULL, G = TRUE, I = TRUE,
       res[m, 1] <- paste0(model, "+G(", k, ")")
       pars <- get_pars(fitG, nseq)
       res[m, 2:6] <- pars
-      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4], preserve.width="individual"),
-                         "\n")
-#      res[m, 2] <- fitG$df
-#      res[m, 3] <- fitG$logLik
-#      res[m, 4] <- AIC(fitG)
-#      res[m, 5] <- AICc(fitG)
-#      res[m, 6] <- AIC(fitG, k = log(nseq))
+      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4],
+                         preserve.width="individual"), "\n")
       calls[[m]] <- fitG$call
       trees[[m]] <- fitG$tree
       m <- m + 1
     }
     if (G & I) {
-#      if (trace > 0) print(paste0(model, "+G+I"))
       fitGI <- update(fitI, k = k)
       fitGI <- optim.pml(fitGI, model = model, optGamma = TRUE,
         optInv = TRUE, control = control)
       res[m, 1] <- paste0(model, "+G(", k, ")+I")
       pars <- get_pars(fitGI, nseq)
       res[m, 2:6] <- pars
-      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4], preserve.width="individual"),
-                         "\n")
-#      res[m, 2] <- fitGI$df
-#      res[m, 3] <- fitGI$logLik
-#      res[m, 4] <- AIC(fitGI)
-#      res[m, 5] <- AICc(fitGI)
-#      res[m, 6] <- AIC(fitGI, k = log(nseq))
+      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4],
+                         preserve.width="individual"), "\n")
       calls[[m]] <- fitGI$call
       trees[[m]] <- fitGI$tree
       m <- m + 1
     }
     if (FREQ) {
-#      if (trace > 0) print(paste0(model, "+F"))
       fitF <- optim.pml(fittmp, model = model, optBf = TRUE,
         control = control)
       res[m, 1] <- paste0(model, "+F")
       pars <- get_pars(fitF, nseq)
       res[m, 2:6] <- pars
-      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4], preserve.width="individual"),
-                         "\n")
-#      res[m, 2] <- fitF$df
-#      res[m, 3] <- fitF$logLik
-#      res[m, 4] <- AIC(fitF)
- #     res[m, 5] <- AICc(fitF)
- #     res[m, 6] <- AIC(fitF, k = log(nseq))
+      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4],
+                         preserve.width="individual"), "\n")
       calls[[m]] <- fitF$call
       trees[[m]] <- fitF$tree
       m <- m + 1
     }
     if (FREQ & I) {
-#      if (trace > 0) print(paste0(model, "+I+F"))
       fitIF <- update(fitF, inv = fitI$inv)
       fitIF <- optim.pml(fitIF, model = model, optBf = TRUE, optInv = TRUE,
         control = control)
       res[m, 1] <- paste0(model, "+I+F")
       pars <- get_pars(fitIF, nseq)
       res[m, 2:6] <- pars
-      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4], preserve.width="individual"),
-                         "\n")
-#      res[m, 2] <- fitIF$df
-#      res[m, 3] <- fitIF$logLik
-#      res[m, 4] <- AIC(fitIF)
-#      res[m, 5] <- AICc(fitIF)
-#      res[m, 6] <- AIC(fitIF, k = log(nseq))
+      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4],
+                                 preserve.width="individual"), "\n")
       calls[[m]] <- fitIF$call
       trees[[m]] <- fitIF$tree
       m <- m + 1
     }
     if (FREQ & G) {
-#      if (trace > 0) print(paste0(model, "+G+F"))
       fitGF <- update(fitF, k = k, shape = fitG$shape)
       fitGF <- optim.pml(fitGF, model = model, optBf = TRUE,
         optGamma = TRUE, control = control)
       res[m, 1] <- paste0(model, "+G(", k, ")+F")
       pars <- get_pars(fitGF, nseq)
       res[m, 2:6] <- pars
-      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4], preserve.width="individual"),
-                         "\n")
-#      res[m, 2] <- fitGF$df
-#      res[m, 3] <- fitGF$logLik
-#      res[m, 4] <- AIC(fitGF)
-#      res[m, 5] <- AICc(fitGF)
-#      res[m, 6] <- AIC(fitGF, k = log(nseq))
+      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4],
+                         preserve.width="individual"), "\n")
       calls[[m]] <- fitGF$call
       trees[[m]] <- fitGF$tree
       m <- m + 1
@@ -260,13 +216,8 @@ modelTest <- function(object, tree = NULL, model = NULL, G = TRUE, I = TRUE,
       res[m, 1] <- paste0(model, "+G(", k, ")+I+F")
       pars <- get_pars(fitGIF, nseq)
       res[m, 2:6] <- pars
-      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4], preserve.width="individual"),
-                         "\n")
-#      res[m, 2] <- fitGIF$df
-#      res[m, 3] <- fitGIF$logLik
-#      res[m, 4] <- AIC(fitGIF)
-#      res[m, 5] <- AICc(fitGIF)
-#      res[m, 6] <- AIC(fitGIF, k = log(nseq))
+      if (trace > 0) cat(formatC(res[m,1], width=12), prettyNum(pars[-4],
+                         preserve.width="individual"), "\n")
       calls[[m]] <- fitGIF$call
       trees[[m]] <- fitGIF$tree
       m <- m + 1
--- inst/tinytest/test_bootstrap.R
+++ inst/tinytest/test_bootstrap.R
@@ -3,6 +3,8 @@ fun <- function(x) NJ(dist.hamming(x))
 tree <- fun(Laurasiatherian)
 
 bs_trees <- bootstrap.phyDat(Laurasiatherian, fun)
+expect_inherits(bs_trees, "multiPhylo")
+
 tree1 <- plotBS(tree, bs_trees, type="none")
 tree2 <- plotBS(tree, bs_trees, type="none", method="TBE")
 
@@ -18,3 +20,10 @@ expect_equal(tree1, tree3)
 nnet2 <- addConfidences(nnet, bs_trees)
 expect_true(is.null(attr(nnet$splits, "confidences")))
 expect_false(is.null(attr(nnet2$splits, "confidences")))
+
+dat <- Laurasiatherian[sample(47, 10)] |> as.character() |> phyDat()
+fit <- pml_bb(dat, "JC", rearrangement="NNI")
+bs_trees_pml <- bootstrap.pml(fit, bs=10, rearrangement="NNI")
+expect_inherits(bs_trees_pml, "multiPhylo")
+
+
