f5db6b33fa6122e11b8374420bc077fc1579c5fa
Klaus Schliep <klaus.schliep@gmail.com>
2023-02-14 20:30:13 +0100

    bug fix

--- inst/tinytest/test_hash.R
+++ inst/tinytest/test_hash.R
@@ -2,7 +2,7 @@ trees <- rmtree(30, 5, rooted=FALSE)
 trees <- .compressTipLabel(trees, ref=sort(trees$tip.label))
 
 tree_u1 <- unique(trees)
-tmp <- sapply(trees, phangorn:::hash_phylo)
+tmp <- hash(trees)
 tree_u2 <- trees[!duplicated(tmp)]
 
 expect_equal(attr(tree_u1, "old.index"), match(tmp, tmp))
