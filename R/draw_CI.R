c4566756cfc891a836ebeaae124bc66f8c5dfcbb
Klaus Schliep <klaus.schliep@gmail.com>
2023-03-21 17:02:07 +0100

    try to fool is.rooted

--- R/draw_CI.R
+++ R/draw_CI.R
@@ -109,6 +109,7 @@ add_ci <- function(tree, trees, col95 = "#FF00004D", col50 = "#0000FF4D",
 {
   lastPP <- get("last_plot.phylo", envir = ape::.PlotPhyloEnv)
   direction <- lastPP$direction
+  trees <- .uncompressTipLabel(trees)
   if(!is_rooted(tree) || !all(is_rooted(trees))) stop("All trees need to be rooted!")
   X <- edge_length_matrix(tree, trees, rooted=TRUE)[, -(seq_along(Ntip(tree)))]
   CI <- apply(X, 2, FUN=\(x)quantile(na.omit(x), probs=c(.025,.25,.75,.975)))
