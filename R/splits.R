fb56404c7e5eae582d27fcaaac72159463792534
Klaus Schliep <klaus.schliep@gmail.com>
2023-03-21 18:05:00 +0100

    more tests

--- R/splits.R
+++ R/splits.R
@@ -232,7 +232,7 @@ as.splits.multiPhylo <- function(x, ...) {
     trivial <- list(...)$trivial
   else trivial <- TRUE
   lx <-  length(x)
-  if(any(is.rooted(x))) x <- unroot(x)
+  x <- unroot(x)
   splits <- prop.part(x)
   splits <- postprocess.prop.part(splits, method="SHORTwise")
   class(splits) <- "list"
