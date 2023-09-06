5a99179de58f0eeac8d3dae47ef9ba103e512545
Klaus Schliep <klaus.schliep@gmail.com>
2023-09-06 17:34:34 +0200

    add several support values to a tree

--- R/plotBS.R
+++ R/plotBS.R
@@ -1,22 +1,35 @@
-support <- function(tree, trees, method="FBP", tol=1e-8, scale=TRUE){
+support <- function(tree, trees, method="FBP", tol=2e-8, scale=TRUE){
   trees <- keep.tip(trees, tree$tip.label)
-  method <- match.arg(method, c("FBP", "TBE", "MCC"))
+  method <- match.arg(method, c("FBP", "TBE", "MCC"), several.ok=TRUE)
+  multi <- ifelse(length(method)>1, TRUE, FALSE)
   tip2root <- ifelse(method=="MCC", TRUE, FALSE)
   if(all(sapply(trees, \(x)!is.null(x$edge.length)))){
     trees <- di2multi(trees, tol=tol) # , tip2root=tip2root)
   }
-  if(method=="FBP" || method=="MCC"){
+  if(multi) X <- matrix(NA, Nnode(tree), length(method),
+                        dimnames = list(NULL, method))
+  if("MCC" %in% method){
     trees <- .uncompressTipLabel(trees) # check if needed
-    if(method=="MCC" && any(!is.rooted(trees)))
+    if(any(!is.rooted(trees)))
       stop("All trees need to be rooted for method 'MCC'!")
-    if (method=="FBP" && any(is.rooted(trees))) trees <- unroot(trees)
     x <- prop.clades(tree, trees)
     x <- (x / length(trees))
     if(!scale) x <- x * 100
+    if(multi) X[, "MCC"] <- x
   }
-  else {
+  if("FBP" %in% method){
+    trees <- .uncompressTipLabel(trees) # check if needed
+    if (any(is.rooted(trees))) trees <- unroot(trees)
+    x <- prop.clades(tree, trees)
+    x <- (x / length(trees))
+    if(!scale) x <- x * 100
+    if(multi) X[, "FBP"] <- x
+   }
+  if("TBE" %in% method){
     x <- transferBootstrap(tree, trees, FALSE, scale=scale)
+    if(multi) X[, "TBE"] <- x
   }
+  if(multi) return(X)
   x
 }
 
@@ -49,6 +62,7 @@ support <- function(tree, trees, method="FBP", tol=1e-8, scale=TRUE){
 #' @param digits integer indicating the number of decimal places.
 #' @param p only plot support values higher than this percentage number
 #' (default is 0).
+#' @param sep seperator between the different methods.
 #' @param \dots further parameters used by \code{plot.phylo}.
 #' @param frame a character string specifying the kind of frame to be printed
 #' around the bootstrap values. This must be one of "none" (the default),
@@ -91,15 +105,17 @@ support <- function(tree, trees, method="FBP", tol=1e-8, scale=TRUE){
 #' @export
 plotBS <- function(tree, trees, type = "phylogram", method="FBP",
                    bs.col = "black", bs.adj = NULL, digits=3, p = 0,
-                   frame = "none", tol=1e-6, ...) {
+                   frame = "none", tol=1e-6, sep = "/", ...) {
   type <- match.arg(type, c("phylogram", "cladogram", "fan", "unrooted",
                             "radial", "none"))
   if(inherits(tree, "pml")) tree <- tree$tree
   if(!inherits(tree, "phylo")) stop("tree must be of class phylo!")
-  method <- match.arg(method, c("FBP", "TBE", "MCC"))
-
+#  method <- match.arg(method, c("FBP", "TBE", "MCC"), several.ok=TRUE)
+# wird in support gecheckt
   if (hasArg(trees)) {
-    x <-support(tree, trees, method="FBP", tol=tol)
+    x <-support(tree, trees, method=method, tol=tol)
+    x <- round(x, digits=digits)
+    if(length(method)>1) x <- apply(x, 1, paste, collapse=sep)
     tree$node.label <- x
   }
   else {
@@ -146,12 +162,14 @@ plotBS <- function(tree, trees, type = "phylogram", method="FBP",
 
 #' @rdname plotBS
 #' @export
-add_support <- function(tree, trees,method="FBP", tol=1e-8,
-                        scale=TRUE, frame="none", ...){
+add_support <- function(tree, trees, method="FBP", tol=1e-8,
+                        scale=TRUE, frame="none", digits=3, sep="/", ...){
   lastPP <- get("last_plot.phylo", envir = .PlotPhyloEnv)
 #  if(!all.equal(lastPP$edge, reorder(tree)$edge))
 #     stop("tree seems to differ from the last plot!")
-  x <- support(tree, trees,method="FBP", tol=tol, scale=scale)
+  x <- support(tree, trees, method=method, tol=tol, scale=scale)
+  x <- round(x, digits=digits)
+  if(length(method)>1) x <- apply(x, 1, paste, collapse=sep)
   drawSupportOnEdges(x, frame=frame, ...)
 }
 
