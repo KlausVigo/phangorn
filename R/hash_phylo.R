e46e2ccf5e398457219b243199b136b2dceb77b6
Klaus Schliep <klaus.schliep@gmail.com>
2025-09-01 09:09:59 +0200

    clean up

--- R/hash_phylo.R
+++ R/hash_phylo.R
@@ -6,11 +6,6 @@ hash <- function (x, ...) UseMethod("hash")
 hash.phylo <- function(x, rooted=FALSE, ...){
   assert_phylo(x, is_rooted = rooted)
   x <- clean_phylo(x, unroot=!rooted, collapse.singles = TRUE, reorder = TRUE)
-#  if(has.singles(x)) x <- collapse.singles(x)
-#  if(!rooted) x <- unroot(x)
-  # assert_phylo(x, is_rooted = TRUE)
-  # if(rooted && !is.rooted(x)) stop("x must be rooted")
-#  x <- reorder(x, "postorder")
   nTips <- as.integer(length(x$tip.label))
   if(rooted){
     res <- .Call('_phangorn_sorted_bipartCPP', x$edge, nTips)
