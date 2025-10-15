b65ba7502df778269feb5c7765e73c80ddedc106
Klaus Schliep <klaus.schliep@gmail.com>
2025-10-15 14:58:31 +0200

    bug fix

--- inst/tinytest/test_terraces.R
+++ inst/tinytest/test_terraces.R
@@ -1,11 +1,11 @@
 data(woodmouse)
 library(rgl)
 fit <- pml_bb(woodmouse, model="JC")
-trees <- unique(fit$bs)
+trees <- unique(c(fit$tree, fit$bs))
 
 n_tree <- length(trees)
 ll <- logLik(fit)
 terrace <- terraces(fit, plot=FALSE)
 
 expect_equal(dim(terrace), c(n_tree, 3L))
-expect_true(all(terrace[,3] <= ll))
+expect_true(all(terrace[,3] <= ll + 1e-6))
