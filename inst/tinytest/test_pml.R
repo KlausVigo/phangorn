1dba00294c074bbb35bc4ff1ffac8379bb2b76a7
Klaus Schliep <klaus.schliep@gmail.com>
2026-04-03 19:44:39 +0200

    fix test

--- inst/tinytest/test_pml.R
+++ inst/tinytest/test_pml.R
@@ -157,13 +157,19 @@ expect_error(pml_bb(dat, model="GTR", method="tipdated"))
     expect_equal(fit.quadrature$shape, shape, tolerance=1e-4)
 
 # test gamma rw
-    fit0 <- pml(treeU1, dat_tmp, k=4, site.rate = "gamma_phangorn")
+    w <- c(.4,.1,.1,.4)
+    g <- discrete.gamma(2,k=4,w=w)
+    tree_U1 <- treeU1
+    tree_U1$edge.length <- tree_U1$edge.length*3
+    fit_T <- pml(tree_U1, dat, k=4, site.rate = "gamma_phangorn", w=w, g=g)
+    weights <- as.vector(1000 * exp(fit_T$siteLik))
+    dat_tmp <- dat
+    attr(dat_tmp, "weight") <- weights
+    fit_T <- update(fit_T, data=dat_tmp)
+    fit0 <- pml(tree_U1, dat_tmp, k=4, site.rate = "gamma_phangorn")
     fit.rw <- optim.pml(fit0, optEdge=FALSE, optGamma = TRUE,
                                 control = pml.control(epsilon=1e-10, trace=0))
-    expect_equal(logLik(fit.rw),
-                 logLik(pml(treeU1, dat_tmp, shape=shape, k=4,
-                            site.rate = "gamma_quadrature")), tolerance=1e-4)
-
+    expect_equal(logLik(fit.rw),  logLik(fit_T), tolerance=1e-4)
 
 # test rate optimisation
     rate <- 2
